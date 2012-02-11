66788251a7749cf891f880cc2505fdb0b21b0989
Rahul Trikha <rahul.trikha@gmail.com>
2012-02-12 00:40:27 +1030

    Added devise views

--- app/controllers/koi/application_controller.rb
+++ app/controllers/koi/application_controller.rb
@@ -2,6 +2,10 @@ module Koi
   class ApplicationController < ActionController::Base
     before_filter :authenticate_admin!, :except => :login
 
+    def after_sign_out_path_for(resource_or_scope)
+      resource_or_scope == :admin ? admin_root_path : super
+    end
+
     def login
       if admin_signed_in?
         redirect_to dashboard_path
