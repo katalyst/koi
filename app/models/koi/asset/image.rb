d47d54eeee4dd0ffd89c104c4149a71fd478c770
Rahul Trikha <rahul.trikha@gmail.com>
2012-02-11 23:29:50 +1030

    Added all models

---
+++ app/models/koi/asset/image.rb
@@ -0,0 +1,21 @@
+module Koi
+  class Image < Asset
+    validates_size_of :data, maximum: KOI_ASSET_IMAGE_SIZE_LIMIT
+    validates_property :mime_type, of: :data, in: KOI_ASSET_IMAGE_MIME_TYPES
+
+    def html_options
+      { width: width, height: height }
+    end
+
+    delegate :width, :height, to: :data
+
+    def title_options
+      "#{ width } x #{ height }px"
+    end
+
+    def dragonfly_options
+      "#{ width }x#{ height }"
+    end
+
+  end
+end
---
+++ app/models/koi/nav_items/root_nav_item.rb
@@ -0,0 +1,21 @@
+module Koi
+  class RootNavItem < NavItem
+    validates :title, presence: true
+
+    def self.root
+      super || RootNavItem.create!(title: "Home", url: "/", key: "home")
+    end
+
+    def is_what?
+      "home"
+    end
+
+    def self.title
+      "Root"
+    end
+
+    def draggable?
+      false
+    end
+  end
+end
