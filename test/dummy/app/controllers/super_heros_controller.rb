589a7b626586d75a52e28a258625956ff9e0c21e
Rahul Trikha <rahul.trikha@gmail.com>
2012-02-12 12:12:10 +1030

    Added super hero example to dummy app

---
+++ test/dummy/app/controllers/super_heros_controller.rb
@@ -0,0 +1,2 @@
+class SuperHerosController < ApplicationController
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,16 @@
 ENV["RAILS_ENV"] = "test"
 
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
-require "rails/test_help"
+require 'rails/test_help'
+require 'factory_girl'
+require 'forgery'
+require 'turn'
+require 'pry'
 
 Rails.backtrace_cleaner.remove_silencers!
 
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
+# Loading factories
+Dir["#{File.dirname(__FILE__)}/dummy/test/factories/**/*.rb"].each { |f| require f }
