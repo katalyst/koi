e00eec445d9b24ec6ea068b0123ce3b8e8251d70
Rahul Trikha <rahul.trikha@gmail.com>
2012-02-21 01:17:28 +1030

    Added devise orm to a initializer

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,7 +1,7 @@
 class Admin < ActiveRecord::Base
-  devise :database_authenticatable, :recoverable,
-         :rememberable, :trackable, :validatable,
-         :registerable
+  # devise :database_authenticatable, :recoverable,
+         # :rememberable, :trackable, :validatable,
+         # :registerable
 
   attr_accessible :email, :password, :password_confirmation,
                   :remember_me, :first_name, :last_name, :role
