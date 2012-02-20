c8524331813fa7d90a4ca5fe515f9e45577f253c
Rahul Trikha <rahul.trikha@gmail.com>
2012-02-21 00:53:23 +1030

    Added Admin Authentication

--- app/controllers/koi/application_controller.rb
+++ app/controllers/koi/application_controller.rb
@@ -1,30 +1,16 @@
-class Admin
-  def god?
-    true
-  end
-end
-
 module Koi
   class ApplicationController < ActionController::Base
     helper :all
     layout 'koi/admin_crud'
 
-    # before_filter :authenticate_admin!, :except => :login
-
-    def after_sign_out_path_for(resource_or_scope)
-      resource_or_scope == :admin ? admin_root_path : super
-    end
-
-    def current_admin
-      new Admin
-    end
+    before_filter :authenticate_admin!, :except => :login
 
     def login
-      # if admin_signed_in?
+      if admin_signed_in?
         redirect_to dashboard_path
-      # else
-        # redirect_to new_admin_session_path
-      # end
+      else
+        redirect_to koi_engine.new_admin_session_path
+      end
     end
   end
 end
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,43 +1,43 @@
 class Admin < ActiveRecord::Base
-    # devise :database_authenticatable, :recoverable,
-    #        :rememberable, :trackable, :validatable,
-    #        :registerable, :stretches => 20
+  devise :database_authenticatable, :recoverable,
+         :rememberable, :trackable, :validatable,
+         :registerable
 
-    attr_accessible :email, :password, :password_confirmation,
-                    :remember_me, :first_name, :last_name, :role
+  attr_accessible :email, :password, :password_confirmation,
+                  :remember_me, :first_name, :last_name, :role
 
-    validates :first_name, :last_name, :email, :role, presence: true
+  validates :first_name, :last_name, :email, :role, presence: true
 
-    ROLES = ["Super", "Admin"]
+  ROLES = ["Super", "Admin"]
 
-    has_crud searchable: [:id, :first_name, :last_name, :email, :role],
-             ajaxable: true, settings: false
+  has_crud searchable: [:id, :first_name, :last_name, :email, :role],
+           ajaxable: true, settings: false
 
-    crud.config do
-      fields role: { type: :roles }
-      config :admin do
-        index fields: [:id, :first_name, :last_name, :email, :role]
-        form  fields: [:first_name, :last_name, :email, :role, :password, :password_confirmation]
-        show  fields: [:first_name, :last_name, :email, :role, :sign_in_count, :current_sign_in_ip,
-                       :last_sign_in_at, :last_sign_in_ip]
-      end
+  crud.config do
+    fields role: { type: :roles }
+    config :admin do
+      index fields: [:id, :first_name, :last_name, :email, :role]
+      form  fields: [:first_name, :last_name, :email, :role, :password, :password_confirmation]
+      show  fields: [:first_name, :last_name, :email, :role, :sign_in_count, :current_sign_in_ip,
+                     :last_sign_in_at, :last_sign_in_ip]
     end
+  end
 
-    def to_s
-      "#{first_name} #{last_name}"
-    end
+  def to_s
+    "#{first_name} #{last_name}"
+  end
 
-    def is?(value)
-      role.eql?(value)
-    end
+  def is?(value)
+    role.eql?(value)
+  end
 
-    def god?
-      is?("Super")
-    end
+  def god?
+    is?("Super")
+  end
 
-  private
+private
 
-    def password_required?
-      new_record? || destroyed? || password.present? || password_confirmation.present?
-    end
+  def password_required?
+    new_record? || destroyed? || password.present? || password_confirmation.present?
+  end
 end
