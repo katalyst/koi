cb1be1d6d106ff8dc441f7b71a15017c128341be
Rahul Trikha <rahul.trikha@gmail.com>
2012-02-21 14:52:23 +1030

    Settings are selected basis of roles now

--- app/controllers/koi/translations_controller.rb
+++ app/controllers/koi/translations_controller.rb
@@ -1,5 +1,12 @@
 module Koi
   class TranslationsController < AdminCrudController
     defaults :route_prefix => ''
+
+  protected
+
+    def collection
+      @translations ||= (current_admin.god? ?
+        end_of_association_chain.all : end_of_association_chain.admin)
+    end
   end
 end
--- app/models/translation.rb
+++ app/models/translation.rb
@@ -1,7 +1,7 @@
 class Translation < ActiveRecord::Base
   has_crud paginate: false, searchable: false, orderable: false
 
-  validates :locale, :label, :key, :value, :field_type, presence: true
+  validates :locale, :label, :key, :value, :field_type, :role, presence: true
   validates :key, :uniqueness => true
 
   default_scope order("`key` ASC")
@@ -12,14 +12,16 @@ class Translation < ActiveRecord::Base
                  "Rich Text" => "rich_text"
                }
 
+  scope :admin, where(role: "Admin")
+
   crud.config do
     fields field_type: { type: :select, data: FieldTypes },
-           value:      { type: :dynamic }
-
+           value:      { type: :dynamic },
+           role:       { type: :select, data: Admin::ROLES }
     config :admin do
       index fields: [:label],
             title: "Settings"
-      form  fields: [:label, :field_type, :key, :value, :hint, :is_proc],
+      form  fields: [:label, :field_type, :key, :value, :hint, :role, :is_proc],
             title: { new: "Create new setting", edit: "Edit setting" }
     end
   end
