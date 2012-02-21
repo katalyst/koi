4113ec756a5426da188dce36bad7c997a4a16898
Rahul Trikha <rahul.trikha@gmail.com>
2012-02-21 16:23:16 +1030

    Updated application template

--- lib/templates/application/app.rb
+++ lib/templates/application/app.rb
@@ -95,7 +95,19 @@ route 'resources :pages'
 create_file 'config/navigation.rb', <<-END
 # -*- coding: utf-8 -*-
 # Configures your navigation
+SimpleNavigation.register_renderer :sf_menu => SfMenuRenderer
 SimpleNavigation::Configuration.run do |navigation|
+end
+END
+
+create_file 'app/renderers/sf_menu_renderer.rb', <<-END
+class SfMenuRenderer < SimpleNavigation::Renderer::List
+  def render(item_container)
+    item_container.dom_class = options.delete(:dom_class) if options.has_key?(:dom_class)
+    item_container.dom_id = options.delete(:dom_id) if options.has_key?(:dom_id)
+    item_container.items.each { |i| p i.html_options = { link: options[:link] } } if options.has_key?(:link)
+    super
+  end
 end
 END
 
