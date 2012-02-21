e5636f94e0be117799d3e0fe21bd7af9ffcf5e29
Rahul Trikha <rahul.trikha@gmail.com>
2012-02-21 11:57:13 +1030

    Fixed application template

--- lib/templates/application/app.rb
+++ lib/templates/application/app.rb
@@ -63,7 +63,7 @@ class ApplicationController < ActionController::Base
 
   # FIXME: Hack to redirect back to admin after admin login
   def after_sign_in_path_for(resource_or_scope)
-    resource_or_scope.is_a? Admin ? koi_engine.root_path : super
+    resource_or_scope.is_a?(Admin) ? koi_engine.root_path : super
   end
 
   # FIXME: Hack to redirect back to admin after admin logout
