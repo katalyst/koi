81404b64c531f19f311de97f315678b877228803
Rahul Trikha <rahul.trikha@gmail.com>
2012-02-23 11:47:19 +1030

    Removed session and password devise layouts

--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -1,6 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  layout :layout_by_resource
+
+protected
+
+  def layout_by_resource
+    if devise_controller? && resource_name == :admin
+      "koi/devise"
+    else
+      "application"
+    end
+  end
+
   # FIXME: Hack to redirect back to admin after admin login
   def after_sign_in_path_for(resource_or_scope)
     resource_or_scope.is_a?(Admin) ? koi_engine.root_path : super
