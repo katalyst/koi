0361dc80b0c215035820efc3220c1ed315ef2494
Rahul Trikha <rahul.trikha@gmail.com>
2012-03-03 12:35:14 +1030

    Added Simple Google Analytics

--- app/controllers/koi/application_controller.rb
+++ app/controllers/koi/application_controller.rb
@@ -1,20 +1,25 @@
+require 'garb'
+
 module Koi
   class ApplicationController < ActionController::Base
     helper :all
     layout 'koi/admin_crud'
-
     before_filter :authenticate_admin!, :except => :login
-
-    def after_sign_out_path_for(resource_or_scope)
-      debugger
-      resource_or_scope == :admin ? admin_root_path : super
-    end
+    before_filter :load_google_analytics_session
 
     def login
       if admin_signed_in?
         redirect_to dashboard_path
       else
         redirect_to koi_engine.new_admin_session_path
+      end
+    end
+
+    def load_google_analytics_session
+      begin
+        @result = Exits.analytics(params[:analytics])
+      rescue Garb::AuthenticationRequest::AuthError
+        @result = false
       end
     end
   end
