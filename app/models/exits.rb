1ec83de5c15aebfbbba62d9583e2bbace27c29e6
Rahul Trikha <rahul.trikha@gmail.com>
2012-03-04 12:02:42 +1030

    Added twitter to dashboard

--- app/models/exits.rb
+++ app/models/exits.rb
@@ -6,6 +6,8 @@ class Exits < ActiveRecord::Base
   metrics :exits, :pageviews, :unique_pageviews
   dimensions :page_path
 
+  default_scope order("unique_pageviews DESC")
+
   def self.profile
     Garb::Session.login(Translation.find_by_key('site.google_analytics.username').value,
                         Translation.find_by_key('site.google_analytics.password').value)
@@ -13,16 +15,16 @@ class Exits < ActiveRecord::Base
                         p.web_property_id = Translation.find_by_key('site.google_analytics.profile_id').value }
   end
 
-
   def self.analytics(reset=nil)
     if reset || Exits.count.eql?(0)
-      profile ||= Exits.profile
-      @result = Exits.results(profile, :limit => 10,
+      @result = Exits.results(profile,
+                       :limit => 10,
                        :offset => 1,
-                       :start_date => (Date.today - 30),
+                       :start_date => (Date.today - 6.months),
                        :end_date => Date.today,
-                       :filters => {:pageviews.gte => 10},
-                       :sort => :pageviews)
+                       :filters => {:unique_pageviews.gte => 200},
+                       :sort => :unique_pageviews)
+      Exits.destroy_all
       @result.each do |result|
         exits = Exits.find_or_create_by_page_path(result.page_path)
         exits.update_attributes(result.attributes)
