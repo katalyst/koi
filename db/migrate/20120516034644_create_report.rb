288b080d5a7b0c2ea5b30fed0854480d6d9aec13
Rahul Trikha <rahul.trikha@gmail.com>
2012-05-16 13:41:32 +0930

    Added basic google analytics report

---
+++ db/migrate/20120516034644_create_report.rb
@@ -0,0 +1,18 @@
+class CreateReport < ActiveRecord::Migration
+  def change
+    create_table :reports do |t|
+      t.decimal :visits, precision: 8, scale: 2
+      t.decimal :unique_pageviews, precision: 8, scale: 2
+      t.decimal :pageviews, precision: 8, scale: 2
+      t.decimal :pageviews_per_visit, precision: 8, scale: 2
+      t.decimal :avg_time_on_site, precision: 8, scale: 2
+      t.decimal :visit_bounce_rate, precision: 8, scale: 2
+      t.decimal :new_visits, precision: 8, scale: 2
+
+      t.date    :start_date
+      t.date    :end_date
+
+      t.timestamps
+    end
+  end
+end
