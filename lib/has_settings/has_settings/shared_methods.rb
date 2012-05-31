fe4c77f9693871b820dddb380d76eaedd6c0912d
Rahul Trikha <rahul.trikha@gmail.com>
2012-05-31 17:56:52 +0930

    Updated settings form

--- lib/has_settings/has_settings/shared_methods.rb
+++ lib/has_settings/has_settings/shared_methods.rb
@@ -1,20 +1,20 @@
 module HasSettings
   module SharedMethods
     def settings
-      Translation.where("`key` LIKE ?", "#{prefix}.%")
+      Translation.where("`key` LIKE ?", "#{settings_prefix}.%")
     end
 
     def setting(key, default=nil)
-      Translation.find_by_key("#{prefix}.#{key}").try(:value) || default
+      Translation.find_by_key("#{settings_prefix}.#{key}").try(:value) || default
     end
 
     def find_or_create_setting(options={})
-      options[:key] = "#{prefix}.#{options[:key]}"
+      options[:key] = "#{settings_prefix}.#{options[:key]}"
       Translation.find_or_create_by_key(options)
     end
 
     def create_setting(options={})
-      options[:key] = "#{prefix}.#{options[:key]}"
+      options[:key] = "#{settings_prefix}.#{options[:key]}"
       Translation.create(options)
     end
   end
