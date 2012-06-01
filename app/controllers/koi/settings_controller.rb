baa32580232f1dd73681473edf9fb15a2c5c2c46
Rahul Trikha <rahul.trikha@gmail.com>
2012-06-01 12:40:01 +0930

    Fixed weird translation setting is_settable conflict

--- app/controllers/koi/settings_controller.rb
+++ app/controllers/koi/settings_controller.rb
@@ -1,6 +1,6 @@
 module Koi
   class SettingsController < TranslationsController
-    defaults :resource_class => Setting
+    defaults resource_class: Setting
 
     def new
       @setting = Setting.new(prefix: params[:prefix])
