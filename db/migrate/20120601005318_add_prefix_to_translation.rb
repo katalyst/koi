34cc1030101b277da4f5eddbc6c1e2bc4aa439b9
Rahul Trikha <rahul.trikha@gmail.com>
2012-06-04 11:03:44 +0930

    Fixed settings key, prefix uniqueness bug

--- db/migrate/20120601005318_add_prefix_to_translation.rb
+++ db/migrate/20120601005318_add_prefix_to_translation.rb
@@ -2,12 +2,12 @@ class AddPrefixToTranslation < ActiveRecord::Migration
   def up
     add_column   :translations, :prefix, :string
     remove_index :translations, :key
-    add_index    :translations, [:prefix, :key], unique: true
+    add_index    :translations, [:locale, :prefix, :key], unique: true
   end
 
   def down
     remove_column :translations, :prefix
-    remove_index  :translations, [:prefix, :key]
+    remove_index  :translations, [:locale, :prefix, :key]
     add_index     :translations, :key, unique: true
   end
 end
