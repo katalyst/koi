4d8633d148bdcb565387564f2cf3943e22534000
Rahul Trikha <rahul.trikha@gmail.com>
2012-06-04 11:03:44 +0930

    Fixed settings key, prefix uniqueness bug

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -2,13 +2,15 @@ class Translation < ActiveRecord::Base
   has_crud paginate: false, searchable: false,
            orderable: false, settings: false
 
-  validates :locale, :label, :key, :value, :field_type, :role, presence: true
-  validates :key, :uniqueness => true
+  validates :locale, :label, :key, :value, :field_type,
+            :role, presence: true
+
+  validates_uniqueness_of :key, scope: :prefix
 
   before_validation :set_default_values
 
   default_scope order("`key` ASC")
-  scope :non_prefixed, where(prefix: nil)
+  scope :non_prefixed, where("prefix IS NULL OR prefix = ''")
 
   FieldTypes = {
                  "String"    => "string",
