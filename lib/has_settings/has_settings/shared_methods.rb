8fa4272b4722c311808eb8b08ec8c14161a20768
Rahul Trikha <rahul.trikha@gmail.com>
2012-06-04 09:32:05 +0930

    Updated uniqueness index for translation table

--- lib/has_settings/has_settings/shared_methods.rb
+++ lib/has_settings/has_settings/shared_methods.rb
@@ -7,14 +7,5 @@ module HasSettings
     def setting(key, default=nil)
       Setting.find_by_prefix_and_key(settings_prefix, key).try(:value) || default
     end
-
-    def find_or_create_setting(key)
-      Setting.find_or_create_by_prefix_and_key(settings_prefix, options[:key])
-    end
-
-    def create_setting(options={})
-      options[:prefix] = settings_prefix
-      Setting.create(options)
-    end
   end
 end
