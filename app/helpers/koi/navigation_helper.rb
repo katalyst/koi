09f47783ce0ffd5db16e7fa040e9f884cffa8462
Rahul Trikha <rahul.trikha@gmail.com>
2012-06-05 10:03:16 +0930

    Fixed menu item cascading

--- app/helpers/koi/navigation_helper.rb
+++ app/helpers/koi/navigation_helper.rb
@@ -1,5 +1,6 @@
 module Koi::NavigationHelper
   def active_item_cascade
+    return [] unless active_navigation_item_key
     NavItem.find_by_id(active_navigation_item_key.gsub("key_", "")).try(:self_and_ancestors)
   end
 end
