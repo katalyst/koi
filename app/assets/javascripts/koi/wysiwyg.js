d1ffe228ecc94e085a3c0608e43213cc1396de4c
Alex Goldsmith <alex.k.goldsmith@gmail.com>
2012-08-17 20:24:10 +0930

    Added asset manager to redactor.

--- app/assets/javascripts/koi/wysiwyg.js
+++ app/assets/javascripts/koi/wysiwyg.js
@@ -1,272 +1,75 @@
-//= require ./lib/wysihtml5
+//= require ./lib/redactor
 
-! function ($, wysihtml5)
+$ (function () // [koi=wysiwyg]
 {
-  function ask (opt, f)
+  $.fn.outerHTML || ($.fn.outerHTML = function ()
   {
-    if (typeof opt === 'string') opt = { question: opt };
+    return $ ('<div>').append (this.clone ()).html ()
+  })
 
-    var app = $ ('[koi=ask]');
-    var sub = app.find ('[name=submit]');
-    var ans = app.find ('[name=answer]')
+  $ ('[koi=wysiwyg]').livequery (function ()
+  {
+    var app = $ (this).redactor
+      ({ fileUpload: '#show-me-the-attachment-button' }).data ().redactor
 
-    for (var k in opt) if (opt.hasOwnProperty (k))
-    ! function (k, v)
+    function nodeName (el)
+    {
+      return el [$.browser.msie ? 'nodeName' : 'tagName']
+    }
+
+    app.getCurrentNodeName = function ()
+    {
+      return nodeName (this.getCurrentNode ())
+    }
+
+    app.getParentNodeName = function ()
+    {
+      return nodeName (this.getParentNode ())
+    }
+
+    app.showImage = function ()
+    {
+      launchAssetManager ('/admin/images/new', function (asset)
       {
-        var el = app.find ('[koi\\:bind=' + k + ']');
-        if (el.is ('input')) el.val (v); else el.html (v);
-      }
-      (k, opt [k]);
-
-    function click ()
-    {
-      app.modal ('hide'); f (ans.val ());
+        console.log (asset)
+        this._imageSet ($ ('<img>', asset).outerHTML (), true)
+      })
     }
 
-    function press (e)
+    app.showFile = function ()
     {
-      if (e.which == 13) click ();
-    }
+      if (this.getParentNodeName () == 'A') this.showLink ()
 
-    app.bind ('keypress', press); sub.bind ('click', click);
-    app.bind ('hidden', function ()
-    {
-      app.unbind ('keypress', press); sub.unbind ('click', click);
-    });
-    app.bind ('shown', function ()
-    {
-      ans.focus ();
-    });
-    app.modal ('show');
-  }
-
-  wysihtml5.commands.chooseLink = {
-
-    exec: function (composer, command)
-    {
-      var anchors = $ (this.state (composer, command));
-      var href = anchors.attr ('href');
-
-      ask ({ question: 'Insert Link', answer: href }, function (href)
+      else launchAssetManager ('/admin/documents/new', function (asset)
       {
-        composer.commands.exec ('createLink', href);
-      });
+        var fileName = asset.href.match (/[^\/]+$/).toString ()
+        this.execCommand ('inserthtml', $ ('<a>', asset).text (fileName).outerHTML ())
+        this.showLink ()
+      })
     }
 
-  , state: function (composer, command)
+    function launchAssetManager (path, ok)
     {
-      return wysihtml5.commands.formatInline.state(composer, command, "A");
-    }
-  }
+      var iframe  = $.factory.iframe (path)
+      var imodal  = $ ('<div class="asset-manager modal fade hide">')
 
-  wysihtml5.commands.chooseMail = {
-
-    exec: function (composer, command)
-    {
-      var anchors = $ (this.state (composer, command));
-      var href = anchors.attr ('href').match (/(mailto:)?(.*)/) [2];
-
-      ask ({ question: 'Insert Mail', answer: href }, function (href)
-      {
-        composer.commands.exec ('createLink', 'mailto:' + href);
-      });
-    }
-
-  , state: function (composer, command)
-    {
-      return wysihtml5.commands.formatInline.state(composer, command, "A");
-    }
-  }
-
-  wysihtml5.commands.chooseAsset = {
-
-    exec: function (composer, command, path)
-    {
-      var imodal  = $ ('<div class="asset-manager modal fade hide-element as-clr">');
-      var iframe  = $.factory.iframe (path);
-
-      imodal.appendTo ('body').modal ({ backdrop: true }).modal ('show').html (iframe);
+      imodal.appendTo ('body').modal ({ backdrop: true }).modal ('show').html (iframe)
 
       iframe.load (function ()
       {
-        var iwindow = iframe.contentWindow ();
-        var iclose  = iwindow.close;
-        iwindow.close = close;
-      });
-
-      function close (asset)
-      {
-        if (asset) composer.commands.exec ('insertAsset', asset);
-
-        imodal.modal ('hide', function ()
+        var iwindow = iframe.contentWindow ()
+        var iclose  = iwindow.close
+        iwindow.close = function (asset)
         {
-          imodal.remove (); iclose.call (iwindow);
-        });
-      }
-    }
-  }
+          if (asset) ok.call (app, asset)
 
-  wysihtml5.commands.insertAsset = {
-
-    exec: function (composer, command, asset)
-    {
-      command = asset.command; delete asset.command;
-      composer.commands.exec (command, asset);
-    }
-  }
-
-  var parserRules = { tags: {} };
-
-  var tags = (' a abbr address area article aside audio'
-             +' b base bdi bdo blockquote body br button'
-             +' canvas caption cite code col colgroup command'
-             +' data datalist dd del details dfn div dl dt'
-             +' em'
-             +' fieldset figcaption figure footer form'
-             +' h1 h2 h3 h4 h5 h6 head header hgroup hr html'
-             +' i iframe img input ins'
-             +' kbd keygen'
-             +' label legend li link'
-             +' map mark math menu meta meter'
-             +' nav noscript'
-             +' object ol optgroup option output'
-             +' p param pre progress'
-             +' q'
-             +' rp rt ruby'
-             +' s samp script section select small source span strong style sub summary sup svg'
-             +' table tbody td textarea tfoot th thead time title tr track'
-             +' u ul'
-             +' var video'
-             +' wbr'
-             ).match (/\S+/g);
-
-  for (var i = 0; i < tags.length; i ++) parserRules.tags [tags [i]] = {
-
-    check_attributes:
-    {
-      'class': 'any', 'style': 'any', 'id': 'any', 'title': 'any'
-    }
-
-  , set_attributes:
-    {
-    }
-  }
-
-  parserRules .tags .a      .check_attributes .href         = 'href'
-  parserRules .tags .a      .check_attributes .rel          = 'any'
-  parserRules .tags .a      .check_attributes .target       = 'any'
-  parserRules .tags .img    .check_attributes .alt          = 'any'
-  parserRules .tags .img    .check_attributes .src          = 'href'
-  parserRules .tags .iframe .check_attributes .src          = 'href'
-  parserRules .tags .iframe .check_attributes .width        = 'numbers'
-  parserRules .tags .iframe .check_attributes .height       = 'numbers'
-  parserRules .tags .iframe   .set_attributes .frameborder  = 0
-  parserRules .tags .iframe   .set_attributes .marginwidth  = 0
-  parserRules .tags .iframe   .set_attributes .marginheight = 0
-  parserRules .tags .table    .set_attributes .cellspacing  = 0
-  parserRules .tags .table    .set_attributes .cellpadding  = 0
-  parserRules .tags .table    .set_attributes .border       = 0
-  parserRules .tags .th     .check_attributes .colspan      = 'any'
-  parserRules .tags .th     .check_attributes .rowspan      = 'any'
-  parserRules .tags .td     .check_attributes .colspan      = 'any'
-  parserRules .tags .td     .check_attributes .rowspan      = 'any'
-  
-  $ ('[koi=wysiwyg]').livequery (function ()
-  {
-    var app      = $ (this)
-    var toolbar  = app.find ('[koi\\:name=toolbar]')
-    var textarea = app.find ('[koi\\:name=textarea]')
-    var form     = app.closest ('form')
-
-    var editor = new wysihtml5.Editor (textarea [0],
-      { toolbar: toolbar [0]
-      , parserRules: parserRules 
-      , stylesheets: ['/assets/koi/wysiwyg.css'] })
-
-    editor.on ('load', function ()
-    {
-      var editable = $ ([editor.currentView.doc.body, editor.currentView.textarea.element]);
-      var viewable = $ ([editor.currentView.iframe,   editor.currentView.textarea.element]);
-      var initialHeight = viewable.height ();
-
-      $ (editor.currentView.doc.body).css ('overflow', 'hidden');
-
-      var win = $ (window);
-
-      var absoluteTop = {
-        top: toolbar.css ('top')
-      , left: toolbar.css ('left')
-      , right: toolbar.css ('right')
-      , bottom: toolbar.css ('bottom')
-      , position: 'absolute'
-      }
-
-      var absoluteBottom = {
-        top: absoluteTop + editable.height () - toolbar.height ()
-      , left: 'auto'
-      , right: toolbar.css ('right')
-      , bottom: 'auto'
-      , position: 'absolute'
-      }
-
-      var toolbarOffset = toolbar.offset ();
-      var appOffset;
-
-      var fixed = {
-        top: 300
-      , left: toolbarOffset.left
-      , position: 'fixed'
-      }
-
-      toolbar.css ({ opacity: 0 });
-
-      function comeHither ()
-      {
-        var scroll = {
-          top: win.scrollTop ()
-        , bottom: win.scrollTop () + win.height ()
+          imodal.modal ('hide', function ()
+          {
+            imodal.remove ()
+            iclose.call (iwindow)
+          })
         }
-
-        var top    = fixed.top + scroll.top;
-        var bottom = top + toolbar.height ();
-
-        var isAbove = top < toolbarOffset.top;
-        var isBelow = bottom > app.offset ().top + app.height ();
-
-        absoluteBottom.top = parseInt (absoluteTop.top) + editable.height () - toolbar.height ();
-
-        toolbar.css (isAbove ? absoluteTop : isBelow ? absoluteBottom : fixed);
-      }
-
-      editable.on ('click focus paste keyup', function ()
-      {
-        $ ('[koi=wysiwyg] [koi\\:name=toolbar]').not (toolbar).animate ({ opacity: 0 });
-
-        viewable.animate ({ height: editable.height () }, 100, function ()
-        {
-          toolbar.css (absoluteTop);
-
-          toolbar.animate ({ opacity: 1 });
-
-          toolbarOffset = toolbar.offset ();
-          toolbarOffset.right = toolbarOffset.left + toolbar.width ();
-          toolbarOffset.bottom = toolbarOffset.top + toolbar.height ();
-
-          appOffset = app.offset ();
-          appOffset.right = appOffset.left + app.width ();
-          appOffset.bottom = appOffset.top + app.height ();
-
-          comeHither (); win.bind ('scroll', comeHither);
-        });
-      });
-      
-      // editable.on ('blur', function ()
-      // {
-      //   win.unbind ('scroll', comeHither);
-      //   toolbar.animate ({ opacity: 0 });
-      // });
-    });
-  });
-
-} (jQuery, wysihtml5);
-
+      })
+    }
+  })
+})
