fd2d97b06e237f821e22b29d3d1e4da373bbf5bc
Alex Goldsmith <alex.k.goldsmith@gmail.com>
2012-08-21 18:05:34 +0930

    Getting wysiwyg to work with IE, bit of refactoring.

---
+++ app/controllers/assets_controller.rb
@@ -0,0 +1,17 @@
+class AssetsController < Koi::CrudController
+
+  def show
+    respond_to do |format|
+      format.html { super }
+      format.all { redirect_to data_path resource.data }
+    end
+  end
+
+  def data_path data
+    return data.url unless data.image?
+    data = data.thumb "#{ params[:width] }x#{ params[:height] }" unless params[:width].blank? && params[:width].blank?
+    data = data.encode params[:format]
+    data.url
+  end
+
+end
