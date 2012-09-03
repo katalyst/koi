dbdd8069bbe2abc0161580f110c81b0c9e6d0e12
Alex Goldsmith <alex.k.goldsmith@gmail.com>
2012-09-03 12:49:58 +0930

    Added scrollable modal with dynamic positioning.

--- app/assets/javascripts/koi/application.js
+++ app/assets/javascripts/koi/application.js
@@ -9,6 +9,8 @@
 
 ! function () {
 
+  var $window = $ (window)
+
   $.livequery.registerPlugin ("html");
 
   $.application.debug = true;
@@ -16,19 +18,38 @@
   // $('.tab-content').application
   //   (function ($this) { $this.children ().maximise ('height'); });
 
-  $('.date.input').application
+  $ ('.date.input').application
     (true, function ($this) { $this.koiDatePicker () });
 
   // $('.rich.text.input').application
   //   (true, function ($this) { $this.koiEditor () });
 
-  $('.sortable.fields').application
+  $ ('.sortable.fields').application
     (true, function ($this) { $this.koiSortable () });
 
-  $('.nested-fields').application
+  $ ('.nested-fields').application
     (true, function ($this) { $this.koiNestedFields () });
 
-  $ (".superfish").application
+  $ ('.superfish').application
     (true, function ($this) { $this.superfish ({ delay:100 }); });
+
+  $ ('.modal').livequery (function ()
+    {
+      $ (this).on ('shown', function () { console.log ('foo')  })
+    })
+
+  var fnModal = $.fn.modal
+
+  $.fn.modal = function ()
+  {
+    var it = $ (this)
+
+    if (it.data ('modal')) return fnModal.apply (this, arguments)
+    
+    fnModal.apply (this, arguments)
+
+    if (it.hasClass ('fade')) it.on ('show', function () { it.css ('top', $window.scrollTop () + 100) })
+    return this
+  }
 
 } (jQuery);
