d52259cf35eb1d4adf94b383f46a285233532562
Alex Goldsmith <alex.k.goldsmith@gmail.com>
2012-10-18 17:02:13 +1030

    Removed last bits of wysihtml5.

--- lib/templates/application/app.rb
+++ lib/templates/application/app.rb
@@ -21,10 +21,6 @@ gem 'i18n-active_record'        , :git => 'git://github.com/svenfuchs/i18n-activ
                                   :branch => 'rails-3.2',
                                   :require => 'i18n/active_record'
 
-# WYSIWYG editor
-gem 'bootstrap-wysihtml5-rails' , :require => 'bootstrap-wysihtml5-rails',
-                                  :git => 'git://github.com/Nerian/bootstrap-wysihtml5-rails.git'
-
 gem 'unicorn'
 
 gem_group :development do
