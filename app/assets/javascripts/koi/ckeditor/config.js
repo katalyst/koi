0d9d68722d6e8236f0e0c2b87ea4070c342974c5
Alex Goldsmith <alex.k.goldsmith@gmail.com>
2013-01-21 12:32:08 +1030

    Added CKEditor back in.

---
+++ app/assets/javascripts/koi/ckeditor/config.js
@@ -0,0 +1,49 @@
+﻿/**
+ * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	
+	// %REMOVE_START%
+	// The configuration options below are needed when running CKEditor from source files.
+	config.plugins = 'basicstyles,dialogui,dialog,clipboard,button,toolbar,list,indent,enterkey,entities,wysiwygarea,fakeobjects,link,pastetext,undo,table,panel,floatpanel,menu,contextmenu,tabletools,tableresize,tab,sourcearea,showborders,showblocks,removeformat,image,horizontalrule,listblock,richcombo,format,blockquote,autogrow,oembed,popup,filebrowser';
+	config.skin = 'moono';
+	// %REMOVE_END%
+
+	config.autoGrow_onStartup = true;
+
+	// Define changes to default configuration here.
+	// For the complete reference:
+	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
+
+	// The toolbar groups arrangement, optimized for a single toolbar row.
+	config.toolbarGroups = [
+		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+		// On the basic preset, clipboard and undo is handled by keyboard.
+		// Uncomment the following line to enable them on the toolbar as well.
+		// { name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+		{ name: 'forms' },
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align' ] },
+		{ name: 'links' },
+		{ name: 'insert' },
+		{ name: 'styles' },
+		{ name: 'colors' },
+		{ name: 'tools' },
+		{ name: 'others' },
+		{ name: 'about' }
+	];
+
+	// The default plugins included in the basic setup define some buttons that
+	// we don't want too have in a basic editor. We remove them here.
+	config.removeButtons = 'Anchor,Underline,Strike,Subscript,Superscript';
+
+	// Considering that the basic setup doesn't provide pasting cleanup features,
+	// it's recommended to force everything to be plain text.
+	config.forcePasteAsPlainText = true;
+
+	// Let's have it basic on dialogs as well.
+	config.removeDialogTabs = 'link:upload;link:target;link:advanced;image:Upload;image:advanced'
+};
