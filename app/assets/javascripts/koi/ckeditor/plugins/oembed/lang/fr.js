77841aa51c060abc482308a2fd3cd8fa40b77dd5
Alex Goldsmith <alex.k.goldsmith@gmail.com>
2013-01-23 18:41:38 +1030

    Replaced media embedder.

---
+++ app/assets/javascripts/koi/ckeditor/plugins/oembed/lang/fr.js
@@ -0,0 +1,11 @@
+// French Translation by https://github.com/wissim
+
+CKEDITOR.plugins.setLang('oembed', 'fr', {
+	title : "Intégrer des contenus multimédia externes. (Photo, Video, Audio, ...)",
+	button : "Insérer des contenus multimédia provenant de nombreux sites.",
+	pasteUrl : "Coller l'URL de partage que vous voulez publier. De nombreux services sont pris en charge tels que : (YouTube, Flickr, Qik, Vimeo, Hulu, Viddler, MyOpera, etc.). Vous pouvez aussi utiliser les URLs courtes.",
+	width : "Max. Largeur:",
+	height : "Max. Hauteur:",
+	invalidUrl : "Merci de fournir une URL valide !",
+	noEmbedCode : "Aucun code d'intégration trouvé ou le site n'est pas supporté !"
+});
