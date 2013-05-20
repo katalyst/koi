7f6e3ae0f15d839eadab353d5fb5b7f4babaf05a
Alex Goldsmith <alex.k.goldsmith@gmail.com>
2013-05-20 16:09:26 +0930

    Settings upload fix.

--- app/controllers/koi/admin_crud_controller.rb
+++ app/controllers/koi/admin_crud_controller.rb
@@ -3,5 +3,17 @@ module Koi
     helper :all
     has_crud :admin => true
     defaults :route_prefix => 'admin'
+
+  protected
+
+    def method_missing key, *sig, &blk
+      if match = /(\w+_|\b)koi_(\w+)_path$/.match(key)
+        prefix, suffix = match.to_a.drop 1
+        koi_engine.send :"#{ prefix }#{ suffix }_path", *sig, &blk
+      else
+        super
+      end
+    end
+       
   end
 end
