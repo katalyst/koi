0c9cff0584c7447e03c15f9dd981ec0745850ec3
Alex Goldsmith <alex.k.goldsmith@gmail.com>
2013-05-29 11:01:23 +0930

    Navigation URL fix.

--- lib/has_navigation/has_navigation/has_navigation.rb
+++ lib/has_navigation/has_navigation/has_navigation.rb
@@ -1,5 +1,6 @@
 module HasNavigation
-  def has_navigation(options={})
+  
+  def has_navigation options = {}
     # Include url helpers to generate default path.
     send :include, Rails.application.routes.url_helpers
     # Include class & instance methods.
@@ -14,20 +15,27 @@ module HasNavigation
     module ClassMethods
     end
 
-    def to_navigator(options={})
-      options.merge!(:navigable => self)
+    def to_navigator options = {}
+      title           = respond_to?(:title) ? title : "#{ self.class.name } - #{ self.id }"
+      url             = polymorphic_path self
+      admin_url       = begin
+                          edit_polymorphic_path [:admin, self]
+                        rescue
+                          Koi::Engine.routes.url_helpers.send :"edit_#{ self.class.name.singularize.parameterize '_' }_path", self
+                        end
+      settings_prefix = settings_prefix if respond_to? :settings_prefix
+
       resource_nav_item = self.resource_nav_item.blank? ? ResourceNavItem.new : self.resource_nav_item
-      resource_nav_item.attributes = options.merge(title: (self.try(:title) || "#{self.class} - #{self.id}"),
-                                                   url: polymorphic_path(self),
-                                                   admin_url: edit_polymorphic_path([:admin, self]),
-                                                   setting_prefix: respond_to?(:settings_prefix) ? settings_prefix : nil)
+      resource_nav_item.attributes = options.merge navigable: self, title: title, url: url, admin_url: admin_url, setting_prefix: settings_prefix
       resource_nav_item
     end
 
-    def to_navigator!(options={})
-      navigator = to_navigator(options)
-      return true if navigator.parent_id.blank?
-      navigator.save ? navigator : false
+    def to_navigator! options = {}
+      navigator = to_navigator options
+      return true      if navigator.parent_id.blank?
+      return navigator if navigator.save
+      return false
     end
   end
+
 end
