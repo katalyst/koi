541a55e8726fb9cea30555b8091ea030b24c11fe
Rahul Trikha <rahul.trikha@gmail.com>
2013-06-17 10:57:13 +0930

    Added basic form to update all settings simultaneously

--- app/controllers/koi/settings_controller.rb
+++ app/controllers/koi/settings_controller.rb
@@ -14,5 +14,13 @@ module Koi
       update! { resource.prefix.present? ? request.referer + '#tab-settings' : { action: :index } }
     end
 
+    def update_multiple
+      params['setting'].keys.each do |id|
+        @setting = Setting.find(id.to_i)
+        @setting.update_attributes(params['setting'][id])
+      end
+      redirect_to(request.referer + '#tab-settings')
+    end
+
   end
 end
