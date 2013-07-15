fe177a4fcc03afee5697061e206ae2bad3e42df3
Chris Teague <chris.teague@katalyst.com.au>
2013-07-15 13:21:39 +0930

    WIP Commit - getting one page view working for settings.

--- test/dummy/app/controllers/news_items_controller.rb
+++ test/dummy/app/controllers/news_items_controller.rb
@@ -1,3 +1,2 @@
 class NewsItemsController < Koi::CrudController
 end
-
--- test/dummy/config/initializers/koi.rb
+++ test/dummy/config/initializers/koi.rb
@@ -1,25 +1,27 @@
 # Koi::Asset::Document.extensions = [:pdf, :doc]
 
 Koi::Menu.items = {
-  "Super Heros" => "/admin/super_heros",
-  "Admins" => "/admin/site_users",
-  "News" => "/admin/news_items",
-  "Categories" => "/admin/categories",
-  "URL History" => "/admin/friendly_id_slugs",
+  "Super Heros"  => "/admin/super_heros",
+  "Admins"       => "/admin/site_users",
+  "News"         => "/admin/news_items",
+  "Categories"   => "/admin/categories",
+  "URL History"  => "/admin/friendly_id_slugs",
   "URL Rewriter" => "/admin/url_rewrites"
 }
 
 Koi::Settings.collection = {
-  banners: { label: "Banners", field_type: "images" },
-  title:   { label: "Title", value: "title" }
+  title:       { label: "Title",       value: "title", field_type: 'text' },
+  description: { label: "Description", value: "title", field_type: 'rich_text' }
 }
 
 Koi::Settings.resource = {
-  banners: { label: "Banners", field_type: "images" }
+  banners:     { label: "Banners",     field_type: "radio",     value: 'Test' },
+  title:       { label: "Title",       field_type: "text",      value: 'Test' },
+  description: { label: "Description", field_type: "rich_text", value: 'Test' }
 }
 
 Koi::Asset::Image.sizes = [
-  { width:    '200' , title: '200 pixels wide' },
-  { width:    '400' , title: '400 pixels wide' },
-  { width:    '600' , title: '600 pixels wide' }
+  { width: '200', title: '200 pixels wide' },
+  { width: '400', title: '400 pixels wide' },
+  { width: '600', title: '600 pixels wide' }
 ]
