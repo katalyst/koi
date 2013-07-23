0849ec835de862bea9cd267afb9b22f90b04d67a
Chris Teague <chris.teague@katalyst.com.au>
2013-07-23 10:38:09 +0930

    Removed images field in favour of file & image

--- test/dummy/config/initializers/koi.rb
+++ test/dummy/config/initializers/koi.rb
@@ -23,16 +23,19 @@ Koi::Settings.collection = {
   category:    { label: "Category",    field_type: "select", data_source: Proc.new { Category.all } },
   options:     { label: "Options",     field_type: "select", data_source: Proc.new { SettingOptions }},
   rad_options: { label: "RadOptions",  field_type: "radio",  data_source: Proc.new { SettingOptions }},
-  chk_options: { label: "ChkOptions",  field_type: "check_boxes", data_source: Proc.new { SettingOptions }}
+  chk_options: { label: "ChkOptions",  field_type: "check_boxes", data_source: Proc.new { SettingOptions }},
+  image:       { label: "Image",       field_type: "image" },
+  document:    { label: "Document",    field_type: "file" }
 }
 
 Koi::Settings.resource = {
   string:    { label: "String",   field_type: "string", value: 'Test' },
   title:     { label: "Title",    field_type: "text",   value: 'Test' },
   rich_text: { label: "RichText", field_type: "rich_text" },
-  images:    { label: "Images",   field_type: "images" },
+  image:     { label: "Image",    field_type: "image" },
   boolean:   { label: "Boolean",  field_type: "boolean", is_required: true },
-  options:   { label: "Options",  field_type: "check_boxes", data_source: Proc.new { SettingOptions }, value: 2 }
+  options:   { label: "Options",  field_type: "check_boxes", data_source: Proc.new { SettingOptions }, value: 2 },
+  document:  { label: "Document", field_type: "file" }
 }
 
 Koi::Asset::Image.sizes = [
