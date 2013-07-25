d5fc8442c02ecb496355ba0f89cfabcd72be128d
Rahul Trikha <rahul.trikha@gmail.com>
2013-07-25 16:52:50 +0930

    Fixed rich text rendering in settings

--- test/dummy/config/initializers/koi.rb
+++ test/dummy/config/initializers/koi.rb
@@ -9,6 +9,12 @@ Koi::Menu.items = {
   "URL Rewriter" => "/admin/url_rewrites"
 }
 
+Koi::Asset::Image.sizes = [
+  { width: '200', title: '200 pixels wide' },
+  { width: '400', title: '400 pixels wide' },
+  { width: '600', title: '600 pixels wide' }
+]
+
 # Look at FieldTypes in app/models/settings.rb for supported types
 
 SettingOptions = {
@@ -17,30 +23,21 @@ SettingOptions = {
   'Option 3' => '3'
 }
 
-Koi::Settings.collection = {
-  options:     { label: "Options",          group: "Group 2", field_type: "select", data_source: Proc.new { SettingOptions }},
-  rad_options: { label: "RadOptions",       group: "Group 2", field_type: "radio",  data_source: Proc.new { SettingOptions }},
+settings = {
   title:       { label: "Title",            group: "SEO", field_type: 'string' },
   description: { label: "Meta Description", group: "SEO", field_type: 'text' },
   keywords:    { label: "Meta Keywords",    group: "SEO", field_type: 'text' },
-  chk_options: { label: "ChkOptions",       group: "Group 3", field_type: "check_boxes", data_source: Proc.new { SettingOptions }},
+
+  input:       { label: "Input",            field_type: 'string' },
+  text:        { label: "Text",             field_type: 'text' },
+  rich_text:   { label: "Rich Text",        field_type: "rich_text" },
+  select:      { label: "Select",           field_type: "select", data_source: Proc.new { SettingOptions } },
+  radio:       { label: "Radio Buttons",    field_type: "radio", data_source: Proc.new { SettingOptions } },
+  checkboxes:  { label: "Check Boxes",      field_type: "check_boxes", data_source: Proc.new { SettingOptions } },
+  boolean:     { label: "Boolean",          field_type: "boolean", is_required: true },
   image:       { label: "Image",            field_type: "image" },
   document:    { label: "Document",         field_type: "file" }
 }
 
-Koi::Settings.resource = {
-  title:       { label: "Title",            group: "SEO", field_type: 'string' },
-  description: { label: "Meta Description", group: "SEO", field_type: 'text' },
-  keywords:    { label: "Meta Keywords",    group: "SEO", field_type: 'text' },
-  rich_text:   { label: "RichText",         group: "Group 2", field_type: "rich_text" },
-  image:       { label: "Image",            group: "Group 2", field_type: "image" },
-  boolean:     { label: "Boolean",          field_type: "boolean", is_required: true },
-  options:     { label: "Options",          field_type: "check_boxes", data_source: Proc.new { SettingOptions }, value: 2 },
-  document:    { label: "Document",         field_type: "file" }
-}
-
-Koi::Asset::Image.sizes = [
-  { width: '200', title: '200 pixels wide' },
-  { width: '400', title: '400 pixels wide' },
-  { width: '600', title: '600 pixels wide' }
-]
+Koi::Settings.collection = settings
+Koi::Settings.resource = settings
