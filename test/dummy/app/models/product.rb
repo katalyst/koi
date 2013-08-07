446cefa28697e842173b459d46c80bf26d2f81ba
Rahul Trikha <rahul.trikha@gmail.com>
2013-08-08 08:06:21 +0930

    Added basic nested form

--- test/dummy/app/models/product.rb
+++ test/dummy/app/models/product.rb
@@ -20,12 +20,14 @@ class Product < ActiveRecord::Base
   Countries = ["Australia", "France", "Germany", "Greece"]
   Colours = ["Red", "Blue", "Green"]
 
-  validates :name, :short_description, :description,
-            :publish_date, :launch_date, :genre_list,
-            :banner, :manual, :size, :colour,
-            presence: true
+  validates :name, presence: true
 
-  validate :at_least_one_country
+  # validates :name, :short_description, :description,
+  #           :publish_date, :launch_date, :genre_list,
+  #           :banner, :manual, :size, :colour,
+  #           presence: true
+
+  # validate :at_least_one_country
 
   def at_least_one_country
     errors.add :countries, "Please select at least one country" if countries.blank? || countries.reject(&:blank?).compact.empty?
