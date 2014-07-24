e53d1aa0dd611b280079d5d7d3c85de88353352d
Rahul Trikha <rahul.trikha@gmail.com>
2014-07-24 17:46:17 +0930

    Added sidekiq polling fix to app generator for new apps

--- lib/templates/application/app.rb
+++ lib/templates/application/app.rb
@@ -298,6 +298,9 @@ default_config = { namespace: "#{@app_name}" }
 
 Sidekiq.configure_server do |config|
   config.redis = default_config
+  # if scheduled jobs execute later than than expected uncomment this next line
+  # this should reduce the polling to 5 seconds
+  # config.poll_interval = 5
 end
 
 Sidekiq.configure_client do |config|
