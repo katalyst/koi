643f2de4864ec4dc39766107a9ad32af2733e7a4
Chris Teague <chris@cteague.com.au>
2014-10-24 16:53:17 +1030

    Fix for settings issue

--- Gemfile
+++ Gemfile
@@ -15,9 +15,7 @@ gem 'koi_config'                , git: 'https://github.com/katalyst/koi_config.g
 
 # i18n ActiveRecord backend
 gem 'i18n-active_record'        , git: 'https://github.com/svenfuchs/i18n-active_record.git',
-                                  branch: 'rails-3.2',
-                                  require: 'i18n/active_record'
-
+                                  branch: 'master'
 gem 'pg'
 gem 'pry'
 gem 'better_errors'
--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -5,5 +5,6 @@ if ActiveRecord::Base.connection.tables.include?("translations")
   I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Cache)
   I18n.cache_store = ActiveSupport::Cache.lookup_store(:memory_store)
 
-  I18n.backend = I18n::Backend::Chain.new(I18n::Backend::Simple.new)
+  I18n::Backend::Chain.send(:include, I18n::Backend::ActiveRecord::Missing)
+  I18n.backend = I18n::Backend::Chain.new(I18n::Backend::ActiveRecord.new, I18n::Backend::Simple.new)
 end
