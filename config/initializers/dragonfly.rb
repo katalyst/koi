e5185b3166259f752820fd76adbaca50c0552342
Rahul Trikha <rahul.trikha@gmail.com>
2015-01-29 15:25:38 +1030

    Merged dragonfly config

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -1,5 +1,20 @@
 require 'dragonfly'
 
+# Configure
+Dragonfly.app.configure do
+  plugin :imagemagick
+
+  verify_urls false
+
+  secret "30b253535467d3d782c7b5c5fc83a4ca061e5b69f2e2c4a9e617e62f981280fe"
+
+  url_format "/media/:job/:name"
+
+  datastore :file,
+    root_path: Rails.root.join('public/system/dragonfly', Rails.env),
+    server_root: Rails.root.join('public')
+end
+
 # Configure Image Assets
 Dragonfly.app(:image).configure do
   plugin :imagemagick
