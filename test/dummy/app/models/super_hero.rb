77ddf4864dd294ce8c1e9596a7d7135066d05b72
dbaines <dbaines@gmail.com>
2015-03-20 14:49:39 +1030

    Moved sample readonly field down to telephone and restored the date field sample

--- test/dummy/app/models/super_hero.rb
+++ test/dummy/app/models/super_hero.rb
@@ -33,7 +33,8 @@ class SuperHero < ActiveRecord::Base
            is_alive:       { type: :boolean },
            powers:         { type: :check_boxes, data: Powers },
            images:         { type: :inline },
-           published_at:   { type: :readonly }
+           published_at:   { type: :date },
+           telephone:      { type: :readonly }
 
     form   fields: [:name, :description, :published_at, :gender, :is_alive, :url,
                     :telephone]
