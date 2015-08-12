8f97db2d54860fbed6a95a03371b63111b8d49ab
Rahul Trikha <rahul.trikha@gmail.com>
2015-08-12 20:04:17 +0930

    Enabled UrlRedirect

--- lib/koi/engine.rb
+++ lib/koi/engine.rb
@@ -9,7 +9,7 @@ module Koi
 
     initializer 'static assets' do |app|
       app.middleware.use ::ActionDispatch::Static, "#{root}/public"
-      # app.middleware.insert_before "Rack::Cache", Koi::UrlRedirect
+      app.middleware.insert_before "Rack::Sendfile", Koi::UrlRedirect
       app.config.assets.precompile += %w(modernizr.js selectivizr.js css3-mediaqueries.js application_bottom.js koi.js koi/nav_items.js koi/assets.js)
     end
 
