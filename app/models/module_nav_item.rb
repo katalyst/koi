ce5ce54a9e5e0c0ee8b625b5a7add8c9cebcd716
Rahul Trikha <rahul.trikha@gmail.com>
2015-12-10 10:03:05 +1030

    Fixed nav items after switching to independent config for inherited models

--- app/models/module_nav_item.rb
+++ app/models/module_nav_item.rb
@@ -1,5 +1,25 @@
 class ModuleNavItem < NavItem
+
+  has_crud searchable: [:id, :title, :url], settings: false
+
   validates :title, :url, :parent, presence: true
+
+  crud.config do
+    fields parent_id:     { type: :hidden },
+           is_hidden:     { type: :boolean },
+           is_mobile:     { type: :boolean },
+           alias_id:      { type: :tree },
+           if:            { type: :code },
+           unless:        { type: :code },
+           method:        { type: :code },
+           highlights_on: { type: :code },
+           content_block: { type: :code }
+
+    config :admin do
+      index fields: [:id, :title, :url]
+      form  fields: [:title, :url, :is_hidden, :is_mobile, :parent_id]
+    end
+  end
 
   def is_what?
     "module"
@@ -8,4 +28,5 @@ class ModuleNavItem < NavItem
   def self.title
     "Module"
   end
+
 end
--- app/models/root_nav_item.rb
+++ app/models/root_nav_item.rb
@@ -1,5 +1,25 @@
 class RootNavItem < NavItem
+
+  has_crud searchable: [:id, :title, :url], settings: false
+
   validates :title, presence: true
+
+  crud.config do
+    fields parent_id:     { type: :hidden },
+           is_hidden:     { type: :boolean },
+           is_mobile:     { type: :boolean },
+           alias_id:      { type: :tree },
+           if:            { type: :code },
+           unless:        { type: :code },
+           method:        { type: :code },
+           highlights_on: { type: :code },
+           content_block: { type: :code }
+
+    config :admin do
+      index fields: [:id, :title, :url]
+      form  fields: [:title, :url, :is_hidden, :is_mobile, :parent_id]
+    end
+  end
 
   def self.root
     super || RootNavItem.create!({ title: "Home", url: "/", key: "home" })
@@ -16,4 +36,5 @@ class RootNavItem < NavItem
   def draggable?
     false
   end
+
 end
