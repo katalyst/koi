1e3e8f443335a8d6f7bb8c498eca8617fb1f5f63
Rahul Trikha <rahul.trikha@gmail.com>
2015-12-10 13:25:16 +1030

    Fixed assets after switching to independent config for inherited models

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,10 +1,17 @@
 class Document < Asset
 
+  has_crud paginate: false, settings: false
+
   dragonfly_accessor :data, app: :file
 
   validates_size_of :data, maximum: Koi::KoiAsset::Document.size_limit
   validates_property :mime_type, of: :data,
     in: Koi::KoiAsset::Document.mime_types, case_sensitive: false
+
+  crud.config do
+    fields data: { type: :file, label: false }, tag_list: { type: :tags }
+    config(:admin) { form fields: [:data] }
+  end
 
   def thumbnail(options={})
     # FIXME: For some read mime_type take a long time to compute
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,10 +1,17 @@
 class Image < Asset
 
+  has_crud paginate: false, settings: false
+
   dragonfly_accessor :data, app: :image
 
   validates_size_of :data, maximum: Koi::KoiAsset::Image.size_limit
   validates_property :mime_type, of: :data,
     in: Koi::KoiAsset::Image.mime_types, case_sensitive: false
+
+  crud.config do
+    fields data: { type: :file, label: false }, tag_list: { type: :tags }
+    config(:admin) { form fields: [:data] }
+  end
 
   def html_options
     { width: width, height: height }
