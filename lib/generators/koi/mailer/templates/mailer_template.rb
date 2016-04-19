83d510c819384e3d307a661ee5eca1d2f1b1fdcb
Matt Croall <mattcroall@Matts-MacBook-Pro.local>
2016-04-19 14:36:02 +0930

    Add configurable email addresses for mailer template

--- lib/generators/koi/mailer/templates/mailer_template.rb
+++ lib/generators/koi/mailer/templates/mailer_template.rb
@@ -3,8 +3,8 @@ class <%= class_name %>Mailer < ActionMailer::Base
   def <%= singular_name %>_created(<%= model_path %>_id)
     @<%= model_path %> = <%= class_name %>.find(<%= model_path %>_id)
     mail subject: "<%= class_name %> Created",
-         from: "no-reply@katalyst.com.au",
-         to: <%= class_name %>.setting(:<%= singular_name %>_emails, "admin@katalyst.com.au",
+         from: Figaro.env.no_reply_address,
+         to: <%= class_name %>.setting(:<%= singular_name %>_emails, Figaro.env.default_to_address,
                 role: "Admin", field_type: "text")
                 .split(",").collect(&:strip)
   end
