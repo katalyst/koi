f1f80a0c933dfa984954480da6429cebe373f7bc
dbaines <dbaines@gmail.com>
2016-07-08 20:16:18 +0930

    Removed column conform script entirely

--- app/assets/javascripts/koi.js
+++ app/assets/javascripts/koi.js
@@ -39,7 +39,6 @@
 //= require koi/components/kat-image-upload
 //= require koi/components/navigation
 //= require koi/components/tooltip
-//= require koi/components/column-conform
 //= require koi/components/pagination-helper
 //= require koi/components/select-link
 //= require koi/components/map
