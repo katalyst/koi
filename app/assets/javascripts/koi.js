23e923611b65ccf126a1a5fb765df63399800a04
dbaines <dbaines@gmail.com>
2016-07-16 23:25:18 +0930

    Refactored the inline nested component to work globally

--- app/assets/javascripts/koi.js
+++ app/assets/javascripts/koi.js
@@ -48,6 +48,7 @@
 //= require koi/components/dropdown
 //= require koi/components/toggle
 //= require koi/components/form-latlng
+//= require koi/components/form-inline-nested
 //= require koi/components/table
 
 //= require koi/ckeditor
