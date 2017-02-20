5165fba3dfe6705f36f3418813ac831d6fcc384a
dbaines <dbaines@gmail.com>
2017-02-20 12:58:10 +1030

    Fixed settings images being a string field rather than inline nested

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -32,7 +32,9 @@ class Setting < Translation
            value:      { type: :dynamic },
            prefix:     { type: :hidden },
            label:      { writable_method: :god? },
-           role:       { type: :select, data: Admin::ROLES }
+           role:       { type: :select, data: Admin::ROLES },
+           is_proc:    { type: :boolean },
+           images:     { type: :inline }
     config :admin do
       index fields: [:label],
             title: "Settings"
