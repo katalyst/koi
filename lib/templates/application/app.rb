33fdefde61f0efa394f145899b240c861a1b9216
Peter Vawser <petervawser@Peters-MacBook-Pro.local>
2017-03-09 14:22:02 +1030

    Fixed broken pages controller template code

--- lib/templates/application/app.rb
+++ lib/templates/application/app.rb
@@ -213,7 +213,23 @@ end
 END
 
 # import Pages controller
-copy_file 'app/controllers/pages_controller.rb'
+create_file 'app/controllers/pages_controller.rb', <<-END
+class PagesController < Koi::CrudController
+
+  # Stop accidental leakage of unwanted actions to frontend
+
+  def index
+    redirect_to '/'
+  end
+
+  alias :index :create
+  alias :index :destroy
+  alias :index :update
+  alias :index :edit
+  alias :index :new
+
+end
+END
 
 run 'bundle install'
 
