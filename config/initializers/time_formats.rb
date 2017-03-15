80252723dc3f18857727fdb910a08bd8489cc75f
bill <bill.j.pearce@gmail.com>
2017-03-15 16:23:58 +1030

    added year to dummy app default datetime format to aid testing filtering

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,3 +1,3 @@
 Time::DATE_FORMATS[:pretty] = lambda { |time| time.strftime("%a, %b %e at %l:%M") + time.strftime("%p").downcase }
 Date::DATE_FORMATS[:default] = "%d %b %Y"
-Time::DATE_FORMATS[:default] = "%a, %b %e at %l:%M %p"
+Time::DATE_FORMATS[:default] = "%a, %b %e, %Y at %l:%M %p"
