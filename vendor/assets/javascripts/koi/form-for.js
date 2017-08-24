74e44a8607f47722f335b6f71d358b0b47c7274c
Peter Vawser <petervawser@Peters-MacBook-Pro.local>
2017-08-24 17:14:33 +0930

    Fix for Nav items duplicating

--- vendor/assets/javascripts/koi/form-for.js
+++ vendor/assets/javascripts/koi/form-for.js
@@ -1,17 +1,14 @@
 (function($) {
   $(function(){
-
     // https://stackoverflow.com/questions/20658402/internet-explorer-issue-with-html5-form-attribute-for-button-element
     // detect if browser supports this
-    var sampleElement = $('[form]').get(0);
-    var isIE11 = !(window.ActiveXObject) && "ActiveXObject" in window;
-    if (sampleElement && window.HTMLFormElement && sampleElement.form instanceof HTMLFormElement && !isIE11) {
-      // browser supports it, no need to fix
-      return;
-    }
-
     $("body").on("click", "button[form]", function(e){
       var $element = $(this);
+      var sampleElement = this;
+      if (sampleElement && window.HTMLFormElement && sampleElement.form instanceof HTMLFormElement) {
+        // browser supports it, no need to fix
+        return;
+      }
       var $form = $("#" + $element.attr("form"));
       $form.submit();
     });
