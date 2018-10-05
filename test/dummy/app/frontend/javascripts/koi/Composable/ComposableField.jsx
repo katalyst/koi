168fb7afd853809ac5e9f77a9059a50e8dee3ee3
dbaines <dbaines@gmail.com>
2018-10-05 15:46:16 +0930

    Composable - allowed components to be defined without any fields

--- test/dummy/app/frontend/javascripts/koi/Composable/ComposableField.jsx
+++ test/dummy/app/frontend/javascripts/koi/Composable/ComposableField.jsx
@@ -26,49 +26,57 @@ export default class ComposableField extends React.Component {
   }
 
   render() {
-    return(
-      <div className="composable--field-template inputs">
-        {this.props.template.map(field => {
-          var key = this.props.parentKey + "__" + field.name;
-          var fieldType = field.type.replace(/_/g, "");
-          var capitalisedFirstType = fieldType.charAt(0).toUpperCase() + fieldType.slice(1);
-          var FieldTypeComponent = ComposableTypes["ComposableField" + capitalisedFirstType];
-          var wrapperClass = field.wrapperClass || "";
-          var fieldId = this.props.parentKey + "__" + field.name;
-          if(field.data && field.data.length) {
-            field.data = this.standardiseData(field.data);
-          }
-          if(FieldTypeComponent) {
-            return(
-              <div className={"control-group " + wrapperClass + " " + field.type} key={key}>
-                {field.label
-                  ? <label className="control-label" htmlFor={fieldId}>{field.label}</label>
-                  : false
-                }
-                {field.hint 
-                  ? <div className="hint-block">
-                      {field.hint}
-                    </div>
-                  : false
-                }
-                <div className="controls">
-                  <FieldTypeComponent fieldSettings={field} 
-                                      value={this.props.data[field.name]} 
-                                      fieldIndex={this.props.fieldIndex} 
-                                      id={fieldId} 
-                                      onChange={this.props.onChange} 
-                  />
-                </div>  
-              </div>
-            );
-          } else {
-            return (
-              <p key={key}>Unsupported field type: {field.type}</p>
-            );
-          }
-        })}
-      </div>
-    );
+    if(this.props.template && this.props.template.length) {
+      return(
+        <div className="composable--field-template inputs">
+          {this.props.template.map(field => {
+            var key = this.props.parentKey + "__" + field.name;
+            var fieldType = field.type.replace(/_/g, "");
+            var capitalisedFirstType = fieldType.charAt(0).toUpperCase() + fieldType.slice(1);
+            var FieldTypeComponent = ComposableTypes["ComposableField" + capitalisedFirstType];
+            var wrapperClass = field.wrapperClass || "";
+            var fieldId = this.props.parentKey + "__" + field.name;
+            if(field.data && field.data.length) {
+              field.data = this.standardiseData(field.data);
+            }
+            if(FieldTypeComponent) {
+              return(
+                <div className={"control-group " + wrapperClass + " " + field.type} key={key}>
+                  {field.label
+                    ? <label className="control-label" htmlFor={fieldId}>{field.label}</label>
+                    : false
+                  }
+                  {field.hint 
+                    ? <div className="hint-block">
+                        {field.hint}
+                      </div>
+                    : false
+                  }
+                  <div className="controls">
+                    <FieldTypeComponent fieldSettings={field} 
+                                        value={this.props.data[field.name]} 
+                                        fieldIndex={this.props.fieldIndex} 
+                                        id={fieldId} 
+                                        onChange={this.props.onChange} 
+                    />
+                  </div>  
+                </div>
+              );
+            } else {
+              return (
+                <p key={key}>Unsupported field type: {field.type}</p>
+              );
+            }
+          })}
+        </div>
+      );
+    } else {
+      return(
+        <div className="composable--field-template inputs">
+          <p>This component doesn't offer any configuration.</p>
+        </div>
+      );
+    }
   }
 }
 
