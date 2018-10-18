f15af405073b9ea2fba9d68993db015c676906ad
dbaines <dbaines@gmail.com>
2018-10-18 11:43:13 +1030

    Reduced simple nav global config to bare minimum and inlined config in to the two koi wrappers to force those settings if they are overridden, added more alarming presentation to the debug wrapper type

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -3,15 +3,19 @@ SimpleForm.setup do |config|
   # Default wrapper which DOES NOT render an input field
   # This is to highlight that the form field NEEDS to have it's wrapper defined as :koi
   config.wrappers :koi_debug, tag: :div do |b|
-    b.wrapper tag: :div, class: :controls do |ba|
+    b.wrapper tag: :div, class: "form__needs-wrapper" do |ba|
       ba.use :label
     end
   end
 
   # Generic input wrapper for all koi input fields
-  config.wrappers :koi, tag: :div, class: 'control-group', error_class: :error, hint_class: :hint do |b|
+  config.wrappers :koi, tag: :div,
+    class: 'control-group',
+    error_class: :error,
+    hint_class: :hint do |b|
+
     b.use :placeholder
-    b.use :label
+    b.use :label, class: 'control-label'
     b.use :hint,  wrap_with: { tag: :p, class: 'hint-block' }
     b.wrapper tag: :div, class: :controls do |ba|
       ba.use :input
@@ -20,7 +24,13 @@ SimpleForm.setup do |config|
   end
 
   # Specific checkbox wrapper to make boolean fields look nicer
-  config.wrappers :koi_checkbox, tag: :div, class: 'control-group checkbox__single', error_class: :error, hint_class: :hint do |b|
+  config.wrappers :koi_checkbox, tag: :div, 
+    class: 'control-group checkbox__single',
+    error_class: :error,
+    hint_class: :hint,
+    boolean_label_class: "checkbox control-label",
+    boolean_style: :nested do |b|
+
     b.use :hint,  wrap_with: { tag: :p, class: 'hint-block' }
     b.wrapper tag: :div, class: :controls do |ba|
       ba.use :label_input
@@ -31,10 +41,7 @@ SimpleForm.setup do |config|
   # Baseline config updates that will get lost if custom simple_form initializer
   # is present
   config.default_wrapper = :koi_debug
-  config.boolean_style = :nested
   config.error_notification_tag = :div
-  config.error_notification_class = 'alert alert-error'
-  config.label_class = 'control-label'
-  config.browser_validations = false
+  config.error_notification_class = 'panel panel__error'
 
 end
