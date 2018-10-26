f15f731c4700f64c881544d46342beb9cdf2d2be
dbaines <dbaines@gmail.com>
2018-10-26 14:59:29 +1030

    Composable - removed fixed methods on library since it's using position sticky now instead, UI improvements for draggable items, fixed glitchy dropping of library items on composition

--- test/dummy/app/frontend/javascripts/koi/Composable/ComposableLibrary.jsx
+++ test/dummy/app/frontend/javascripts/koi/Composable/ComposableLibrary.jsx
@@ -5,60 +5,33 @@ export default class ComposableLibrary extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      fixed: false,
-    };
-    this.stickyLibrary = this.stickyLibrary.bind(this);
-    this.debouncedStickyLibrary = this.debouncedStickyLibrary.bind(this);
-  }
-
-  componentDidMount(){
-    // window.addEventListener("scroll", this.debouncedStickyLibrary, { passive: true });
-  }
-
-  componentWillUnmount(){
-    // window.removeEventListener("scroll", this.debouncedStickyLibrary);
-  }
-
-  stickyLibrary() {
-    const offset = 64; // offset for header + gap
-    const scrollPosition = window.pageYOffset + offset;
-    const elementTop = this.$element.offsetTop;
-    const shouldBeFixed = scrollPosition > elementTop;
-    if(this.state.fixed !== shouldBeFixed) {
-      this.setState({
-        fixed: shouldBeFixed
-      });
-    }
-  }
-
-  debouncedStickyLibrary() {
-    window.requestAnimationFrame(this.stickyLibrary);
   }
 
   render() {
     return(
-      <div className={`composable--library ${this.state.fixed ? "composable--library__fixed" : ""}`} ref={el => this.$element = el}>
+      <div className={`composable--library`} ref={el => this.$element = el}>
         <Droppable droppableId="library" isDropDisabled={true}>
           {(libraryDroppableProvided, libraryDroppableSnapshot) => (
-            <div ref={libraryDroppableProvided.innerRef} className="spacing-xxx-tight">
+            <div ref={libraryDroppableProvided.innerRef}>
               {this.props.composableTypes.map((component, index) => {
                 return(
-                  <div key={component.slug}>
-                    <Draggable
-                      draggableId={component.slug}
-                      index={index}
-                    >
-                      {(libraryDraggableProvided, libraryDraggableSnapshot) => (
+                  <Draggable
+                    draggableId={component.slug}
+                    index={index}
+                    key={component.slug}
+                  >
+                    {(libraryDraggableProvided, libraryDraggableSnapshot) => (
+                      <div
+                        className="composable--component--wrapper"
+                        ref={libraryDraggableProvided.innerRef}
+                        {...libraryDraggableProvided.draggableProps}
+                        {...libraryDraggableProvided.dragHandleProps}
+                      >
                         <div
-                          ref={libraryDraggableProvided.innerRef}
                           className={`
                             composable--library--component 
                             ${libraryDraggableSnapshot.isDragging ? "composable--component__dragging" : ""}
                           `}
-                          {...libraryDraggableProvided.draggableProps}
-                          {...libraryDraggableProvided.dragHandleProps}
-
                         >
                           {this.props.helpers.icons &&
                             <React.Fragment>
@@ -78,9 +51,9 @@ export default class ComposableLibrary extends React.Component {
                             {component.name || component.slug}
                           </div>
                         </div>
-                      )}
-                    </Draggable>
-                  </div>
+                      </div>
+                    )}
+                  </Draggable>
                 )
               })}
               {libraryDroppableProvided.placeholder}
