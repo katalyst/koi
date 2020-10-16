e72b844c8ecf93e54da1a9a3745a114f92a5d36b
Stephen Nelson <stephen@katalyst.com.au>
2020-10-16 17:17:07 +1030

    Avoid duplicating default settings in the database

--- lib/has_settings/has_settings/shared_methods.rb
+++ lib/has_settings/has_settings/shared_methods.rb
@@ -11,22 +11,15 @@ module HasSettings
         prefix: settings_prefix
       })
 
-      setting = Setting.find_by_prefix_and_key(settings_prefix, key) || Setting.create(options)
+      setting = Setting.find_by_prefix_and_key(settings_prefix, key)
 
-      value = case setting.field_type.to_s
-              when "check_boxes"
-                setting.serialized_value
-              when "file"
-                setting.file
-              when "image"
-                setting.file
-              when "boolean"
-                !!setting.value.to_s.eql?("1")
-              when "tags"
-                setting.tags
-              else
-                setting.value
-              end
+      setting = if setting.nil? && options != default_setting_options(key)
+                  Setting.create(options)
+                else
+                  Setting.new(options)
+                end
+
+      setting_value(setting)
     end
 
     def create_setting(key, values)
@@ -44,5 +37,33 @@ module HasSettings
       []
     end
 
+    private
+
+    def default_setting_options(key)
+      {
+        key:    key.to_s,
+        label:  key.to_s.titleize,
+        field_type: 'rich_text',
+        prefix: settings_prefix,
+        value:  nil
+      }
+    end
+
+    def setting_value(setting)
+      case setting.field_type.to_s
+      when "check_boxes"
+        setting.serialized_value
+      when "file"
+        setting.file
+      when "image"
+        setting.file
+      when "boolean"
+        !!setting.value.to_s.eql?("1")
+      when "tags"
+        setting.tags
+      else
+        setting.value
+      end
+    end
   end
 end
