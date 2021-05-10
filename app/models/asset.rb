29505271d1ed2ca9ab80abcaf3d4a87984e12e9a
Andy Williams <andy.williams@katalyst.com.au>
2021-05-10 11:33:53 +0930

    Update scoped_search to 4.1.8

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -4,7 +4,7 @@ class Asset < ActiveRecord::Base
 
   dragonfly_accessor :data, app: :file
 
-  scoped_search :on => [:data_name]
+  scoped_search on: [:data_name]
 
   scope :unassociated, -> { where("attributable_type IS NULL OR attributable_type = ''") }
   scope :search_data,  -> query { where("data_name LIKE ?", "%#{query}%") }
--- lib/has_crud/has_crud/active_record.rb
+++ lib/has_crud/has_crud/active_record.rb
@@ -118,7 +118,7 @@ module HasCrud
             setup_default_searchable
           end
 
-          scoped_search :on => self.options[:searchable]
+          scoped_search on: self.options[:searchable]
         end
       end
 
