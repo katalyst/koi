ce93af41c68ebc7cd012264fdfd85a3417101bdd
Stephen Nelson <stephen@katalyst.com.au>
2021-05-14 15:47:08 +0930

    Fix: Rails 5 changed `belongs_to` default to required
    Assets do not require an attributable relation.

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -12,7 +12,7 @@ class Asset < ActiveRecord::Base
 
   acts_as_ordered_taggable
 
-  belongs_to :attributable, polymorphic: true
+  belongs_to :attributable, polymorphic: true, optional: true
 
   validates_presence_of :data
   validates_property :mime_type, of: :data,
