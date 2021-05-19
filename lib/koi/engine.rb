db500fe3698cf81dbc7c5157253a0273f8226665
Andy Williams <andy.williams@katalyst.com.au>
2021-05-19 15:56:17 +0930

    Revert earlier change to assets precompile list

--- lib/koi/engine.rb
+++ lib/koi/engine.rb
@@ -9,6 +9,10 @@ module Koi
       app.middleware.use ::ActionDispatch::Static, "#{root}/public"
       app.middleware.use Koi::UrlRedirect
       app.config.assets.precompile += %w(
+        koi.js
+        koi/nav_items.js
+        koi/assets.js
+        koi/application.css
         koi/modernizr.js
         koi/selectivizr.js
         koi/css3-mediaqueries.js
