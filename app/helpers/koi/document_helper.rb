cfce1f7db66d8f5b8ead764cd0a7d5685cf2b2f9
Andy Williams <andy.williams@katalyst.com.au>
2021-05-19 16:50:59 +0930

    Fix for crash when document data_name is nil

--- app/helpers/koi/document_helper.rb
+++ app/helpers/koi/document_helper.rb
@@ -1,7 +1,7 @@
 module Koi::DocumentHelper
 
   def document_thumbnail(document, options={})
-    ext = File.extname(document.data_name).gsub('.', '')
+    ext = document.data_name ? File.extname(document.data_name).gsub('.', '') : ""
     return path_to_asset(Koi::KoiAsset::Document.icons[ext]) if Koi::KoiAsset::Document.icons.has_key?(ext)
     return path_to_asset(Koi::KoiAsset::Document.icons['img']) if Koi::KoiAsset::Image.extensions.include?(ext.to_sym)
     path_to_asset(Koi::KoiAsset.unknown_image)
