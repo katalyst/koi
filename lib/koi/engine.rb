55eb37a113ecc35b045b42d57a053c1e5b4fb156
Ananya Rama Murthy <ananya.murthy@katalyst.com.au>
2021-05-26 12:59:42 +0930

    Remove unused js libraries

--- lib/koi/engine.rb
+++ lib/koi/engine.rb
@@ -14,8 +14,6 @@ module Koi
         koi/assets.js
         koi/application.css
         koi/modernizr.js
-        koi/selectivizr.js
-        koi/css3-mediaqueries.js
         koi/ckeditor.js
       )
     end
