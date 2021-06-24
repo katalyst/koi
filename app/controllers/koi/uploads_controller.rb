2fa22bef89f39a622d0c7ea69b2e7999b3cfee30
Andy Williams <andy.williams@katalyst.com.au>
2021-06-24 15:42:06 +0930

    KOI-33: Fix ckeditor "Send it to the server" button

--- app/controllers/koi/uploads_controller.rb
+++ app/controllers/koi/uploads_controller.rb
@@ -2,11 +2,23 @@ module Koi
   class UploadsController < AdminCrudController
     skip_before_action :verify_authenticity_token # TODO[asw]: send CSRF token for uploads
 
+    # params[:upload] is present if the "Send it to the server" button was pressed.
+    # params[:file] is present if the data was uploaded by kat-image-upload.js
     def create
-      asset = asset_class.new
-      asset.data = params[:file]
+      asset      = asset_class.new
+      asset.data = params[:upload] || params[:file]
       asset.save
-      render plain: asset.id
+
+      if params[:upload]
+        response = {
+          "uploaded": 1,
+          "fileName": asset.data_name,
+          "url":      asset.url,
+        }
+        render json: response
+      else
+        render plain: asset.id
+      end
     end
 
     private
