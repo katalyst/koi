2f53bbf261997c57a84d807c71588cb35ab47316
Andy Williams <andy.williams@katalyst.com.au>
2021-06-28 09:55:46 +0930

    Add storage_backend setting

--- lib/koi/koi_asset.rb
+++ lib/koi/koi_asset.rb
@@ -2,12 +2,28 @@ require 'active_support/core_ext/numeric/bytes'
 
 module Koi
   module KoiAsset
-    # Image incase no icon cannot be found
-    mattr_accessor :unknown_image
-    @@unknown_image = 'koi/application/icon-file-unknown.png'
+    class << self
+      # Image to use when no icon cannot be found
+      attr_accessor :unknown_image
 
-    # Default file upload size limit
-    mattr_accessor :size_limit
-    @@size_limit = 5.megabytes
+      # Default file upload size limit
+      attr_accessor :size_limit
+
+      # Back end file storage provider for images and documents.
+      # one of :dragonfly, :active_storage, :dragonfly_active_storage (used for migrating)
+      attr_accessor :storage_backend
+
+      def use_dragonfly?
+        %i[dragonfly dragonfly_active_storage].include?(storage_backend)
+      end
+
+      def use_active_storage?
+        %i[active_storage dragonfly_active_storage].include?(storage_backend)
+      end
+    end
+
+    @unknown_image   = 'koi/application/icon-file-unknown.png'
+    @size_limit      = 5.megabytes
+    @storage_backend = defined?(Dragonfly::Model) ? :dragonfly : :active_storage
   end
 end
