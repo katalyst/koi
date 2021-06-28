3fc566b163c8ddd6a952350e1481072e2eb6bab6
Andy Williams <andy.williams@katalyst.com.au>
2021-06-28 09:48:09 +0930

    Refactor Koi::Settings
    removed use of @@ class variables (rubocop warning)

--- lib/koi/settings.rb
+++ lib/koi/settings.rb
@@ -1,15 +1,18 @@
 module Koi
   module Settings
-    # Settings for all collections
-    mattr_accessor :collection
-    @@collection = {}
+    class << self
+      # Settings for all collections
+      attr_accessor :collection
 
-    # Settings for all resources
-    mattr_accessor :resource
-    @@resource = {}
+      # Settings for all resources
+      attr_accessor :resource
 
-    # Skip settings on create for all these resources
-    mattr_accessor :skip_on_create
-    @@skip_on_create = []
+      # Skip settings on create for all these resources
+      attr_accessor :skip_on_create
+    end
+
+    @collection = {}
+    @resource = {}
+    @skip_on_create = []
   end
 end
