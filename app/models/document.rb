033ab64d9f3ff2cdb8c2c3c3cc2fc5c92d26bf3c
Andy Williams <andy.williams@katalyst.com.au>
2021-08-02 13:15:01 +0930

    Use vanity urls for documents
    Changed document url to be /documents/:id instead of linking directly to s3.
    Matches behaviour of images.

--- app/models/document.rb
+++ app/models/document.rb
@@ -13,8 +13,16 @@ class Document < Asset
     config(:admin) { form fields: [:data] }
   end
 
-  def url(*args)
-    data.url
+  def thumbnail(options={})
+    # FIXME: For some read mime_type take a long time to compute
+    #        Using ext to make the document list faster
+    ext = File.extname(data_name).gsub('.', '')
+    return Koi::KoiAsset::Document.icons[ext] if Koi::KoiAsset::Document.icons.has_key?(ext)
+    return Koi::KoiAsset::Document.icons['img'] if Koi::KoiAsset::Image.extensions.include?(ext.to_sym)
+    Koi::KoiAsset.unknown_image
   end
 
+  def url(*args)
+    "/#{ self.class.to_s.tableize }/#{ to_param }.#{ data.ext }"
+  end
 end
