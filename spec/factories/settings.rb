ffd5875e2c65217b3611fe3b63c445f756ce3c89
Andy Williams <andy.williams@katalyst.com.au>
2022-06-15 14:33:00 +0930

    Add spec test for HasSettings

---
+++ spec/factories/settings.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :setting do
+    string
+
+    key { SecureRandom.uuid }
+    label { SecureRandom.uuid }
+    role { "Admin" }
+    locale { "en" }
+    prefix { "page" }
+
+    trait :string do
+      field_type { "string" }
+      value { Faker::Lorem.word }
+    end
+
+    trait :boolean do
+      field_type { "boolean" }
+      value { "1" }
+    end
+
+    trait :text do
+      field_type { "text" }
+      value { Faker::Lorem.sentence }
+    end
+
+    trait :rich_text do
+      field_type { "rich_text" }
+      value { Faker::Lorem.sentence }
+    end
+  end
+end
