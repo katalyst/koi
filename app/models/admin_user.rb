385e1a0e7b802096da8371283edfbee7f7de0b7e
Sughosh Rao <sughosh.rao@katalyst.com.au>
2022-07-13 11:53:19 +0930

    Fix: Specs locally and on github actions

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class AdminUser < ApplicationRecord
+  include HasCrud::ActiveRecord
+
   class << self
     def model_name
       ActiveModel::Name.new(self, nil, "Admin")
