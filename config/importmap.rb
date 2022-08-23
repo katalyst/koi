d7b8a73e9a357f4b3948c67b0316198fd5beb25b
Stephen Nelson <stephen@katalyst.com.au>
2022-08-23 16:35:45 +0930

    Add turbo for admin pages

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,5 +2,6 @@
 
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 
 pin_all_from Koi::Engine.root.join("app/assets/javascripts/koi/controllers"), under: "koi/controllers"
