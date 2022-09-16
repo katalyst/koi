76af27cac2a9f4f487f55ee7fd9df83c587eede3
Stephen Nelson <stephen@katalyst.com.au>
2022-09-16 12:20:12 +0930

    Sluggable stimulus controller automatically sets slugs
    If there is no slug set, create one as the user types.

---
+++ app/assets/javascripts/koi/controllers/sluggable_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from "@hotwired/stimulus";
+
+/**
+ * Connect an input (e.g. title) to slug.
+ */
+export default class SluggableController extends Controller {
+  static targets = ["source", "slug"];
+  static values = {
+    slug: String,
+  }
+
+  sourceChanged(e) {
+    if (this.slugValue === "") {
+      this.slugTarget.value = parameterize(this.sourceTarget.value);
+    }
+  }
+
+  slugChanged(e) {
+    this.slugValue = this.slugTarget.value;
+  }
+}
+
+function parameterize(input) {
+  return input.toLowerCase()
+    .replace(/'/g, '-')
+    .replace(/[^-\w\s]/g, '')
+    .replace(/[^a-z0-9]+/g, '-')
+    .replace(/(^-|-$)/g, '');
+}
