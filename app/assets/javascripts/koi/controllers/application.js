9fc6057d0c29a21ea40284a13ad4af79251cde10
Stephen Nelson <stephen@katalyst.com.au>
2022-09-21 22:40:26 +0930

    FRIN23-19: Move trix dependency to content gem

--- app/assets/javascripts/koi/controllers/application.js
+++ app/assets/javascripts/koi/controllers/application.js
@@ -2,7 +2,6 @@ import { Application } from "@hotwired/stimulus"
 
 // Stimulus controllers. This should ultimately be moved to koi/admin.js
 import "@hotwired/turbo-rails"
-import "trix";
 import "@rails/actiontext";
 
 const application = Application.start()
