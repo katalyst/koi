609c5e06b8760be66aaa027b4d235233d82a275c
Stephen Nelson <stephen@katalyst.com.au>
2022-11-30 21:46:59 +1030

    Fix missed references to defunct sitemap controller

--- app/controllers/koi/legacy_pages_controller.rb
+++ app/controllers/koi/legacy_pages_controller.rb
@@ -3,11 +3,5 @@
 module Koi
   class LegacyPagesController < AdminCrudController
     defaults route_prefix: ""
-
-    def destroy
-      super do |format|
-        format.html { redirect_to sitemap_nav_items_path }
-      end
-    end
   end
 end
