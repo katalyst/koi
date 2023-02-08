4c761ebabf04cb5a1c89cd20eb3ceacff420dc9c
Stephen Nelson <stephen@katalyst.com.au>
2023-02-08 20:15:23 +1030

    Remove dragonfly assets and controllers (to FRG)

--- app/models/document.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Document < Asset
-  dragonfly_accessor :data, app: :file
-
-  def url(*_args)
-    "/#{self.class.to_s.tableize}/#{to_param}.#{data.ext}"
-  end
-end
