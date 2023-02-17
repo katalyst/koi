f783d56a15f1e251b2ed7501cfef85c15fe37ed7
Ananya Rama Murthy <ananya.murthy@katalyst.com.au>
2023-02-17 12:28:10 +1030

    Add page css

---
+++ spec/dummy/config/initializers/dartsass.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+Rails.application.config.dartsass.builds = {
+  # Koi assets
+  "koi/koi-admin.scss" => "admin.css",
+  "application.scss"   => "application.css",
+}
