2dce79cee1446090c5171fa0f73eb76456f896c3
Ananya Rama Murthy <ananya.murthy@katalyst.com.au>
2023-02-17 14:55:29 +1030

    Hack: images need host?

--- spec/dummy/config/environment.rb
+++ spec/dummy/config/environment.rb
@@ -5,3 +5,6 @@ require File.expand_path("application", __dir__)
 
 # Initialize the rails application
 Dummy::Application.initialize!
+
+# Set the default host and port to be the same as Action Mailer.
+Rails.application.routes.default_url_options = Rails.application.config.action_mailer.default_url_options
