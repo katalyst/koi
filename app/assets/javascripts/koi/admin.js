19289e86771dec7362f1e5fa88542e98c00d7e86
Stephen Nelson <stephen@katalyst.com.au>
2023-02-23 16:47:52 +1030

    Remove ornament CSS and icons
    Preparing for re-skinning and simplified Koi admin layouts.

--- app/assets/javascripts/koi/admin.js
+++
@@ -1,6 +0,0 @@
-// HIGH PRIORITY JAVASCRIPT INCLUDED AT TOP OF PAGE
-
-//= require jquery
-
-//= require koi/ornament/defaults
-//= require koi/components/navigation-mobile
--- app/helpers/koi/application_helper.rb
+++ app/helpers/koi/application_helper.rb
@@ -3,18 +3,5 @@
 module Koi
   module ApplicationHelper
     include Katalyst::Tables::Frontend
-
-    private
-
-    # Icon Helper
-    # <%= icon("close", width: 24, height: 24, stroke: "#BADA55", fill: "purple") %>
-    def icon(icon_path, options = {})
-      options[:width] = 24 if options[:width].blank?
-      options[:height] = 24 if options[:height].blank?
-      options[:stroke] = "#000000" if options[:stroke].blank?
-      options[:fill] = "#000000" if options[:fill].blank?
-      options[:class] = "" if options[:class].blank?
-      render("koi/shared/icons/#{icon_path}", options:)
-    end
   end
 end
