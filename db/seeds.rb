4df5eab5431d2ea934769b5253fb109a3b8a4b69
Alan Cornthwaite <alan.cornthwaite@katalyst.com.au>
2023-05-29 13:13:53 +0930

    Remove admin user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,1 @@
 # frozen_string_literal: true
-
-require "securerandom"
-require "thor"
-thor = Thor::Shell::Basic.new
-
-unless AdminUser.find_by(email: "admin@katalyst.com.au")
-  password = SecureRandom.base58(16)
-
-  thor.say("Your admin@katalyst.com.au password has been set to #{password}, please update your password manager",
-           :green)
-
-  # Default Super admin
-  AdminUser.create(first_name: "Admin", last_name: "Katalyst", email: "admin@katalyst.com.au", role: "Super", password:)
-end
