2d545cf8d26b4c0dfecffdf2fd5c5b1d91ceb0e4
Stephen Nelson <stephen@katalyst.com.au>
2023-09-04 13:22:25 +0930

    Remove deprecated default date and time formatters
    time.to_s(:default) has been deprecated since Rails 7 and will be
    removed in Rails 7.1.

--- config/initializers/time_formats.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-Time::DATE_FORMATS[:pretty] = ->(time) { time.strftime("%a, %b %e at %l:%M") + time.strftime("%p").downcase }
-Date::DATE_FORMATS[:default] = "%d %b %Y"
-Time::DATE_FORMATS[:default] = "%a, %b %e at %l:%M %p"
