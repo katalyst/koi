ed8fb0acdfd62fe30a992fc82255b86910444258
Hasarinda Thenuwara <hasarinda.thenuwara@katalyst.com.au>
2023-12-11 14:39:33 +1030

    Fix: TableComponent renders when pagination is disabled

--- app/components/koi/index_table_component.rb
+++ app/components/koi/index_table_component.rb
@@ -2,20 +2,24 @@
 
 module Koi
   class IndexTableComponent < ViewComponent::Base
-    attr_reader :table, :pagination, :id, :pagy_id
+    attr_reader :id, :pagy_id
+
+    renders_one :table, Katalyst::Turbo::TableComponent
+    renders_one :pagination, Katalyst::Turbo::PagyNavComponent
 
     def initialize(collection:, id: "index-table", **options)
       super
 
-      @id = id
+      @id      = id
       @pagy_id = "#{id}-pagination"
-      @table = Katalyst::Turbo::TableComponent.new(collection:, id:, class: "index-table", caption: true, **options)
-      @pagination = Katalyst::Turbo::PagyNavComponent.new(collection:, id: pagy_id) if collection.paginate?
+
+      with_table(collection:, id:, class: "index-table", caption: true, **options)
+      with_pagination(collection:, id: pagy_id) if collection.paginate?
     end
 
     def call
-      concat(table.render_in(view_context))
-      concat(pagination.render_in(view_context)) if pagination
+      concat(table)
+      concat(pagination)
     end
   end
 end
