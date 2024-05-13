5ef478825371a6e83f81de16454a3cf39ecf3770
Alan Cornthwaite <alan.cornthwaite@katalyst.com.au>
2024-05-13 15:12:32 +0930

    Remove unusable components

--- app/components/koi/tables/table_component.rb
+++ app/components/koi/tables/table_component.rb
@@ -5,8 +5,6 @@ module Koi
     # Custom table component, in order to override the default header and body row components
     # which enables us to use our own custom header and body cell components
     class TableComponent < Katalyst::TableComponent
-      config.header_row = "Koi::Tables::HeaderRowComponent"
-      config.body_row = "Koi::Tables::BodyRowComponent"
     end
   end
 end
