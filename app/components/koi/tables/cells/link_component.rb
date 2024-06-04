4028eca2c066864bddf18b4c278eee04b02a6993
Stephen Nelson <stephen@katalyst.com.au>
2024-06-04 13:00:50 +0930

    Change TableComponent#link to use a content wrapper
    This allows users to provide alternative content without overwriting
    the link_to call.

--- app/components/koi/tables/cells/link_component.rb
+++ app/components/koi/tables/cells/link_component.rb
@@ -6,36 +6,54 @@ module Koi
       # Displays a link to the record
       # The link text is the value of the attribute
       class LinkComponent < Katalyst::Tables::CellComponent
-        define_html_attribute_methods :link_attributes
-
-        def initialize(url:, link:, **)
+        def initialize(url:, default_url:, link:, **)
           super(**)
 
-          @url = url
-
-          self.link_attributes = link
-        end
-
-        def rendered_value
-          link_to(value, url, **link_attributes)
-        end
-
-        def url
-          case @url
-          when Symbol
-            # helpers are not available until the component is rendered
-            @url = helpers.public_send(@url, record)
-          when Proc
-            @url = @url.call(record)
-          else
-            @url
-          end
+          with_content_wrapper(WrapperComponent.new(cell: self, url:, default_url:, **link))
         end
 
         private
 
         def default_html_attributes
           { class: "type-link" }
+        end
+
+        class WrapperComponent < ViewComponent::Base
+          include Katalyst::HtmlAttributes
+
+          delegate :record, :column, :value, to: :@cell
+
+          def initialize(cell:, url:, default_url:, **)
+            super(**)
+
+            @cell = cell
+            @url = url
+            @default_url = default_url
+          end
+
+          def call
+            link_to(content, @default_url ? default_url : url, **html_attributes)
+          end
+
+          def url
+            case @url
+            when Symbol
+              # helpers are not available until the component is rendered
+              @url = helpers.public_send(@url, record)
+            when Proc
+              @url = @url.call(record)
+            else
+              @url
+            end
+          end
+
+          def default_url
+            if value.is_a?(ApplicationRecord)
+              [:admin, value]
+            else
+              [:admin, record]
+            end
+          end
         end
       end
     end
