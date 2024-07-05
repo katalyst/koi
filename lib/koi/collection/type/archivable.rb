264d2e7c93e50a3e4fb2911123ae09c330abdb84
Stephen Nelson <stephen@katalyst.com.au>
2024-07-05 11:34:28 +0930

    Improve documentation for archivable query filter

--- lib/koi/collection/type/archivable.rb
+++ lib/koi/collection/type/archivable.rb
@@ -6,16 +6,28 @@ module Koi
       # Add support for `attribute :status, :archivable, default: :active` to
       # Koi collections to support filtering on Koi::Model::Archivable models.
       class Archivable < Katalyst::Tables::Collection::Type::Enum
-        def initialize
-          super(multiple: false, scope: :status)
+        def initialize(scope: :status)
+          super(multiple: false, scope:)
         end
 
         def type
           :archivable
         end
 
-        def examples_for(...)
-          %i[active archived all]
+        def examples_for(scope, attribute)
+          _, model, = model_and_column_for(scope, attribute)
+
+          %i[active archived all].map do |key|
+            example(key, describe_key(model, attribute, key))
+          end
+        end
+
+        private
+
+        def describe_key(model, attribute, key)
+          description = I18n.t("koi.view.#{attribute.name}.#{key}", model: model.model_name.human)
+          description += " (default)" if key.to_s.eql?(attribute.original_value.to_s)
+          description
         end
       end
     end
