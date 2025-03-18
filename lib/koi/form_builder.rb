8403676735e8757bf2f3bf4d5aca92621b750673
Stephen Nelson <stephen@katalyst.com.au>
2025-03-18 13:03:31 +1030

    Content 3.0

--- lib/koi/form_builder.rb
+++ lib/koi/form_builder.rb
@@ -4,5 +4,6 @@ module Koi
   class FormBuilder < ActionView::Helpers::FormBuilder
     include GOVUKDesignSystemFormBuilder::Builder
     include Koi::Form::Builder
+    include Koi::Form::Content
   end
 end
