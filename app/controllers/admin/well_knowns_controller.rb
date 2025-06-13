0ae126e3801034257e15ed48e2853ce37172e8e0
Stephen Nelson <stephen@katalyst.com.au>
2025-06-14 00:18:28 +0930

    Update WellKnowns using generator

--- app/controllers/admin/well_knowns_controller.rb
+++ app/controllers/admin/well_knowns_controller.rb
@@ -4,58 +4,60 @@ module Admin
   class WellKnownsController < ApplicationController
     before_action :set_well_known, only: %i[show edit update destroy]
 
+    attr_reader :collection, :well_known
+
     def index
-      collection = Collection.new.with_params(params).apply(::WellKnown.strict_loading.all)
+      @collection = Collection.new.with_params(params).apply(WellKnown.strict_loading.all)
 
       render locals: { collection: }
     end
 
     def show
-      render locals: { well_known: @well_known }
+      render locals: { well_known: }
     end
 
     def new
-      render locals: { well_known: ::WellKnown.new }
+      @well_known = WellKnown.new
+
+      render locals: { well_known: }
     end
 
     def edit
-      render locals: { well_known: @well_known }
+      render locals: { well_known: }
     end
 
     def create
-      @well_known = ::WellKnown.new(well_known_params)
+      @well_known = WellKnown.new(well_known_params)
 
       if @well_known.save
-        redirect_to [:admin, @well_known], status: :see_other
+        redirect_to admin_well_known_path(well_known), status: :see_other
       else
-        render :new, locals: { well_known: @well_known }, status: :unprocessable_content
+        render :new, locals: { well_known: }, status: :unprocessable_content
       end
     end
 
     def update
-      if @well_known.update(well_known_params)
-        redirect_to action: :show, status: :see_other
+      if well_known.update(well_known_params)
+        redirect_to admin_well_known_path(well_known), status: :see_other
       else
-        render :edit, locals: { well_known: @well_known }, status: :unprocessable_content
+        render :edit, locals: { well_known: }, status: :unprocessable_content
       end
     end
 
     def destroy
       @well_known.destroy!
 
-      redirect_to action: :index, status: :see_other
+      redirect_to admin_well_knowns_path, status: :see_other
     end
 
     private
 
-    # Only allow a list of trusted parameters through.
-    def well_known_params
-      params.expect(well_known: %i[name purpose content_type content])
-    end
-
-    # Use callbacks to share common setup or constraints between actions.
     def set_well_known
       @well_known = ::WellKnown.find(params[:id])
+    end
+
+    def well_known_params
+      params.expect(well_known: %i[name purpose content_type content])
     end
 
     class Collection < Admin::Collection
@@ -64,7 +66,7 @@ module Admin
 
       attribute :name, :string
       attribute :purpose, :string
-      attribute :content_type, :string
+      attribute :content_type, :enum
       attribute :content, :string
     end
   end
