8a2faa656044eac8e6883ad760dd724ee69f1dac
Stephen Nelson <stephen@katalyst.com.au>
2025-06-14 00:18:28 +0930

    Update WellKnowns using generator

--- spec/requests/admin/well_knowns_controller_spec.rb
+++ spec/requests/admin/well_knowns_controller_spec.rb
@@ -4,12 +4,12 @@ require "rails_helper"
 
 RSpec.describe Admin::WellKnownsController do
   let(:admin) { create(:admin) }
-  let(:model) { create(:well_known) }
+  let(:well_known) { create(:well_known) }
 
   include_context "with admin session"
 
   describe "GET /admin/well_knowns" do
-    let(:action) { get polymorphic_path([:admin, WellKnown]) }
+    let(:action) { get admin_well_knowns_path }
 
     it_behaves_like "requires admin"
 
@@ -17,52 +17,10 @@ RSpec.describe Admin::WellKnownsController do
       action
       expect(response).to have_http_status(:success)
     end
-
-    context "with a large collection" do
-      before { create_list(:well_known, 25) } # rubocop:disable FactoryBot/ExcessiveCreateList
-
-      it "paginates the collection" do
-        action
-        expect(response.body).to have_css("tbody tr", count: 20)
-      end
-    end
-
-    context "with sort parameter" do
-      let(:action) { get polymorphic_path([:admin, WellKnown], sort: "name desc") }
-
-      before do
-        create(:well_known, name: "first")
-        create(:well_known, name: "second")
-      end
-
-      it "finds first in second place" do
-        action
-        expect(response.body).to have_css("tbody tr + tr td", text: "first")
-      end
-    end
-
-    context "with search parameter" do
-      let(:action) { get polymorphic_path([:admin, WellKnown], search: "first") }
-
-      before do
-        create(:well_known, name: "first")
-        create(:well_known, name: "second")
-      end
-
-      it "finds the needle" do
-        action
-        expect(response.body).to have_css("table td", text: "first")
-      end
-
-      it "removes the chaff" do
-        action
-        expect(response.body).to have_no_css("table td", text: "second")
-      end
-    end
   end
 
   describe "GET /admin/well_knowns/new" do
-    let(:action) { get new_polymorphic_path([:admin, WellKnown]) }
+    let(:action) { get new_admin_well_known_path }
 
     it_behaves_like "requires admin"
 
@@ -73,14 +31,14 @@ RSpec.describe Admin::WellKnownsController do
   end
 
   describe "POST /admin/well_knowns" do
-    let(:action) { post polymorphic_path([:admin, WellKnown]), params: { well_known: params } }
+    let(:action) { post admin_well_knowns_path, params: { well_known: params } }
     let(:params) { attributes_for(:well_known) }
 
     it_behaves_like "requires admin"
 
     it "renders successfully" do
       action
-      expect(response).to redirect_to([:admin, assigns(:well_known)])
+      expect(response).to redirect_to(admin_well_known_path(assigns(:well_known)))
     end
 
     it "creates a well_known" do
@@ -89,7 +47,7 @@ RSpec.describe Admin::WellKnownsController do
   end
 
   describe "GET /admin/well_knowns/:id" do
-    let(:action) { get polymorphic_path([:admin, model]) }
+    let(:action) { get admin_well_known_path(well_known) }
 
     it_behaves_like "requires admin"
 
@@ -100,7 +58,7 @@ RSpec.describe Admin::WellKnownsController do
   end
 
   describe "GET /admin/well_knowns/:id/edit" do
-    let(:action) { get edit_polymorphic_path([:admin, model]) }
+    let(:action) { get edit_admin_well_known_path(well_known) }
 
     it_behaves_like "requires admin"
 
@@ -111,26 +69,26 @@ RSpec.describe Admin::WellKnownsController do
   end
 
   describe "PATCH /admin/well_knowns/:id" do
-    let(:action) { patch polymorphic_path([:admin, model]), params: { well_known: params } }
+    let(:action) { patch admin_well_known_path(well_known), params: { well_known: params } }
     let(:params) { attributes_for(:well_known) }
 
     it_behaves_like "requires admin"
 
     it "renders successfully" do
       action
-      expect(response).to redirect_to(polymorphic_path([:admin, model]))
+      expect(response).to redirect_to(admin_well_known_path(well_known))
     end
   end
 
   describe "DELETE /admin/well_knowns/:id" do
-    let(:action) { delete polymorphic_path([:admin, model]) }
-    let!(:model) { create(:well_known) }
+    let(:action) { delete admin_well_known_path(well_known) }
+    let!(:well_known) { create(:well_known) }
 
     it_behaves_like "requires admin"
 
     it "renders successfully" do
       action
-      expect(response).to redirect_to(polymorphic_path([:admin, WellKnown]))
+      expect(response).to redirect_to(admin_well_knowns_path)
     end
 
     it "deletes the well_known" do
