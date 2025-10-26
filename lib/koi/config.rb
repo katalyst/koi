61e8c574f41fac7750c1c72146aa365e3344dc66
Stephen Nelson <stephen@katalyst.com.au>
2025-10-27 10:15:59 +1030

    Rails 8.1

--- lib/koi/config.rb
+++ lib/koi/config.rb
@@ -1,32 +1,29 @@
 # frozen_string_literal: true
 
-require "active_support/configurable"
 require "active_support/core_ext/numeric"
 
 module Koi
   class Config
-    include ActiveSupport::Configurable
+    attr_accessor :admin_name,
+                  :authenticate_local_admins,
+                  :resource_name_candidates,
+                  :admin_stylesheet,
+                  :admin_javascript_entry_point,
+                  :document_mime_types,
+                  :document_size_limit,
+                  :image_mime_types,
+                  :image_size_limit
 
-    config_accessor(:admin_name) { "Koi" }
-
-    config_accessor(:authenticate_local_admins) { Rails.env.development? }
-
-    config_accessor(:resource_name_candidates) { %i[title name] }
-
-    config_accessor(:admin_stylesheet) { "admin" }
-
-    config_accessor(:admin_javascript_entry_point) { "@katalyst/koi" }
-
-    config_accessor(:document_mime_types) do
-      %w[image/png image/gif image/jpeg image/webp application/pdf audio/*].freeze
+    def initialize
+      @admin_name = "Koi"
+      @authenticate_local_admins = Rails.env.development?
+      @resource_name_candidates = %i[title name]
+      @admin_stylesheet = "admin"
+      @admin_javascript_entry_point = "@katalyst/koi"
+      @document_mime_types = %w[image/png image/gif image/jpeg image/webp application/pdf audio/*].freeze
+      @document_size_limit = 10.megabytes
+      @image_mime_types = %w[image/png image/gif image/jpeg image/webp].freeze
+      @image_size_limit = 10.megabytes
     end
-
-    config_accessor(:document_size_limit) { 10.megabytes }
-
-    config_accessor(:image_mime_types) do
-      %w[image/png image/gif image/jpeg image/webp].freeze
-    end
-
-    config_accessor(:image_size_limit) { 10.megabytes }
   end
 end
